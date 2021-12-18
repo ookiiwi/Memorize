428ee91111880f36df2a69c6262329ba71a91976
Zakariya Hadrami <zakariyah.dev@gmail.com>
2021-12-18 20:15:49 +0100

    Implement file explorer to replace local implementaion in  list tab

--- lib/widget.dart
+++ lib/widget.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:memorize/data.dart';
 
 class Selectable extends StatefulWidget {
   const Selectable({
     Key? key,
     required this.tag,
-    required this.tagsSelected,
+    required this.onSelected,
     required this.child,
     required this.selectable,
-    this.clear = false,
+    this.clear = true,
   }) : super(key: key);
 
   final int tag;
-  final List<int> tagsSelected;
+  final void Function(int tag, bool value) onSelected;
   final Widget child;
   final bool selectable;
   final bool clear;
@@ -26,16 +27,14 @@ class _Selectable extends State<Selectable> {
 
   void _changeCheckBoxValue({bool? value}) {
     _selected = value ?? !_selected;
-    _selected
-        ? widget.tagsSelected.add(widget.tag)
-        : widget.tagsSelected.remove(widget.tag);
+    widget.onSelected(widget.tag, _selected);
   }
 
   Widget _ignore() {
     if (widget.clear) {
       _selected = false;
     }
-    ;
+
     return Container();
   }
 
@@ -65,5 +64,97 @@ class _Selectable extends State<Selectable> {
                     )),
           ],
         ));
+  }
+}
+
+class FileExplorer extends StatefulWidget {
+  const FileExplorer(
+      {required this.cd,
+      required this.getWdId,
+      this.floatingActionButton,
+      this.onSelection,
+      this.onSelected,
+      this.enableSelection,
+      Key? key})
+      : super(key: key);
+
+  final int Function() getWdId;
+  final void Function(int) cd;
+  final Widget? floatingActionButton;
+  final void Function()? onSelection;
+  final void Function(int tag, bool value)? onSelected;
+  final bool Function()? enableSelection;
+
+  @override
+  State<FileExplorer> createState() => _FileExplorer();
+}
+
+class _FileExplorer extends State<FileExplorer> {
+  final List<int> _selection = [];
+  bool _selectable = false;
+
+  bool _enableSelection() =>
+      widget.enableSelection != null ? widget.enableSelection!() : true;
+
+  Widget _buildItem(int id) {
+    if (!_enableSelection()) {
+      _selectable = false;
+    }
+
+    var item = UserData.get(id);
+    return Selectable(
+        tag: id,
+        onSelected: (id, value) =>
+            widget.onSelected != null ? widget.onSelected!(id, value) : () {},
+        selectable: _selectable,
+        clear: true,
+        child: GestureDetector(
+            onTap: () {
+              if (UserData.getTypeFromId(id) == DataType.category) {
+                setState(() => widget.cd(id));
+              }
+            },
+            onLongPress: () => setState(() {
+                  _selectable = _enableSelection();
+                  if (_selectable && widget.onSelection != null) {
+                    widget.onSelection!();
+                  }
+                }),
+            child: Card(
+                margin: const EdgeInsets.all(10.0),
+                color: (UserData.getTypeFromId(id) == DataType.category
+                    ? Colors.grey
+                    : Colors.amber),
+                child: Center(
+                  child: Text(item != null ? item.name : ''),
+                ))));
+  }
+
+  @override
+  Widget build(BuildContext ctx) {
+    List<int> idsTable = List.from(UserData.get(widget.getWdId()).getTable());
+
+    return Scaffold(
+      body: Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: Column(
+            children: [
+              Expanded(
+                  child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithMaxCrossAxisExtent(
+                        maxCrossAxisExtent: 150,
+                        childAspectRatio: 1,
+                        crossAxisSpacing: 1,
+                        mainAxisSpacing: 1,
+                      ),
+                      itemCount: idsTable.length,
+                      itemBuilder: (BuildContext ctx, int i) {
+                        return _buildItem(idsTable[i]);
+                      }))
+            ],
+          )),
+      floatingActionButton: widget.floatingActionButton,
+    );
   }
 }
