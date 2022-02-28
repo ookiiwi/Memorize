266160990f04afcd0899d69e11247eb7748a1e3a
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-02-28 16:27:37 +0100

    Refactor list explorer and implement nested navigators

--- lib/tab.dart
+++ lib/tab.dart
@@ -1,293 +1,234 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:memorize/data.dart';
-import 'package:memorize/widget.dart';
 
-class ATab {
-  ATab({required Icon icon, required Widget child, bool isMain = false})
-      : _icon = icon,
-        tabIcon = isMain ? const Icon(Icons.home) : icon,
-        tab = child,
-        bMain = isMain;
+class TabNavigator extends StatelessWidget {
+  const TabNavigator(
+      {required this.navigatorKey,
+      required this.builder,
+      Key? key,
+      this.onWillPop})
+      : super(key: key);
 
-  final Icon _icon;
-  Icon tabIcon;
-  Widget tab;
-  bool bMain;
-}
-
-class CommunityTab extends StatefulWidget {
-  const CommunityTab({Key? key}) : super(key: key);
+  final GlobalKey<NavigatorState> navigatorKey;
+  final WidgetBuilder builder;
+  final Future<bool> Function()? onWillPop;
 
   @override
-  State<CommunityTab> createState() => _CommunityTab();
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: () async {
+          return onWillPop != null ? await onWillPop!() : false;
+        },
+        child: Navigator(
+          key: navigatorKey,
+          onGenerateRoute: (settings) {
+            return MaterialPageRoute(builder: (context) {
+              return builder(context);
+            });
+          },
+        ));
+  }
 }
 
-class _CommunityTab extends State<CommunityTab> {
-  //final double _scrollableCategoryHeight = 100.0;
-  //final double _scrollableCategoryMargin = 10.0;
-  final double _scrollableCatHeight = 0.4;
+class ListExplorer extends StatefulWidget {
+  const ListExplorer({Key? key}) : super(key: key);
 
-  Widget _buildSearchBar() {
-    return Container(
-      height: 40.0,
-      margin: const EdgeInsets.symmetric(horizontal: 80.0, vertical: 30.0),
-      //decoration: const BoxDecoration(color: Colors.amber),
-      //child:
-      //const Padding(
-      //  padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 20.0),
-      child: TextField(
-        textAlignVertical: TextAlignVertical.bottom,
-        decoration: InputDecoration(
-          prefixIcon: const Icon(
-            Icons.search,
-            color: Colors.black54,
-          ),
-          hintText: "Search ...",
-          hintStyle: TextStyle(
-            color: AppData.colors["hintText"],
-            fontSize: 20.0,
-          ),
-          filled: true,
-          //fillColor: Color(0xFFF3F3F3),
-          fillColor: AppData.colors["container"],
-          border: OutlineInputBorder(
-            borderSide: BorderSide(
-                color: AppData.colors["border"] ?? const Color(0xFF000000),
-                style: BorderStyle.solid),
-            borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-          ),
-        ),
-      ),
-      //),
-    );
+  @override
+  State<ListExplorer> createState() => _ListExplorer();
+}
+
+class _ListExplorer extends State<ListExplorer> {
+  SortType _sortType = SortType.rct;
+  List<String> _items = [];
+  bool _openBtnMenu = false;
+  late BuildContext _navCtx;
+
+  List<String> tabs = ["recent", "ascending", "descending"];
+
+  @override
+  void initState() {
+    super.initState();
   }
 
+  Widget _buildAddBtn() => FloatingActionButton(
+        heroTag: "listMenuBtn",
+        onPressed: () {
+          setState(() => _openBtnMenu = !_openBtnMenu);
+        },
+        child: Icon(_openBtnMenu ? Icons.cancel : Icons.add),
+      );
+
   @override
-  Widget build(BuildContext cxt) {
+  Widget build(BuildContext ctx) {
+    _items = FileExplorer.listCurrentDir(sortType: _sortType);
+    assert(tabs.length <= SortType.values.length);
+
+    return TabNavigator(
+        onWillPop: () async {
+          if (Navigator.of(_navCtx).canPop()) {
+            Navigator.of(_navCtx).pop();
+          } else {
+            setState(() {
+              FileExplorer.cd('..');
+            });
+          }
+          return false;
+        },
+        navigatorKey: GlobalKey<NavigatorState>(),
+        builder: (context) {
+          _navCtx = context;
+          return Stack(
+            children: [
+              Column(children: [
+                //search bar
+                Container(
+                    margin: const EdgeInsets.all(20),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20))),
+                    )),
+
+                //tab row
+                Container(
+                  height: 50,
+                  color: Colors.amber,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: tabs.length,
+                      itemBuilder: (ctx, i) {
+                        return GestureDetector(
+                            onTap: () =>
+                                setState(() => _sortType = SortType.values[i]),
+                            child: Container(
+                              height: 40,
+                              width: 110,
+                              margin: i < (tabs.length - 1)
+                                  ? const EdgeInsets.only(right: 10)
+                                  : EdgeInsets.zero,
+                              color: Colors.blue,
+                              child: Center(child: Text(tabs[i])),
+                            ));
+                      }),
+                ),
+
+                Container(
+                  color: Colors.purple,
+                  child: Text(FileExplorer.current),
+                ),
+
+                //page view
+                Expanded(
+                    child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.red),
+                        child: PageView.builder(
+                            itemCount: 1,
+                            itemBuilder: (ctx, i) {
+                              return Container(
+                                color: _sortType == SortType.rct
+                                    ? Colors.white
+                                    : (_sortType == SortType.asc
+                                        ? Colors.yellow
+                                        : Colors.green),
+                                child: GridView.builder(
+                                    gridDelegate:
+                                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                                      maxCrossAxisExtent: 150.0,
+                                      mainAxisSpacing: 10.0,
+                                      crossAxisSpacing: 10.0,
+                                      childAspectRatio: 1.0,
+                                    ),
+                                    itemCount: _items.length,
+                                    itemBuilder: (context, i) {
+                                      return GestureDetector(
+                                          onTap: () {
+                                            if (FileExplorer.getFileType(
+                                                    _items[i]) ==
+                                                Directory) {
+                                              setState(() =>
+                                                  FileExplorer.cd(_items[i]));
+                                            } else {
+                                              Navigator.of(ctx).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          ListPage(_items[i])));
+                                            }
+                                          },
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(20),
+                                                color: Colors.indigo),
+                                            child: Center(
+                                                child: Text(
+                                                    FileExplorer.stripPath(
+                                                            _items[i])
+                                                        .last)),
+                                          ));
+                                    }),
+                              );
+                            }))),
+              ]),
+              Positioned(
+                  bottom: 10,
+                  right: 10,
+                  child: !_openBtnMenu
+                      ? _buildAddBtn()
+                      : Column(children: [
+                          FloatingActionButton(
+                            heroTag: "dirAddBtn",
+                            onPressed: () {
+                              //print('add');
+                              setState(() {
+                                _openBtnMenu = !_openBtnMenu;
+                                FileExplorer.createDirectory('test1');
+                              });
+                            },
+                            child: const Icon(Icons.folder),
+                          ),
+                          FloatingActionButton(
+                            heroTag: "listAddBtn",
+                            onPressed: () {
+                              setState(() {
+                                _openBtnMenu = !_openBtnMenu;
+                                AList tmp = AList('list2');
+                                FileExplorer.createList(tmp);
+                              });
+                            },
+                            child: const Icon(Icons.list),
+                          ),
+                          _buildAddBtn(),
+                        ]))
+            ],
+          );
+        });
+  }
+}
+
+class ListPage extends StatefulWidget {
+  const ListPage(String listPath, {Key? key}) : super(key: key);
+
+  @override
+  State<ListPage> createState() => _ListPage();
+}
+
+class _ListPage extends State<ListPage> {
+  @override
+  Widget build(BuildContext ctx) {
     return Column(
       children: [
-        //search bar
-        Expanded(
-            child: ListView(
-          children: [
-            _buildSearchBar(),
-            //Top lists
-            ScrollableCategory(
-                label: RichText(
-                  text: TextSpan(
-                      text: "Top ",
-                      style: const TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                      children: [
-                        TextSpan(
-                            text: 'lists',
-                            style: TextStyle(
-                                color: AppData.colors["buttonSelected"])),
-                      ]),
-                ),
-                height:
-                    MediaQuery.of(context).size.width * _scrollableCatHeight,
-                onTap: (i) => print("top item $i"),
-                itemBuilder: (ctx, i) {
-                  return Container();
-                }),
-
-            //Recent lists
-            Container(
-                margin: const EdgeInsets.only(top: 40.0),
-                child: ScrollableCategory(
-                    label: RichText(
-                      text: TextSpan(
-                          text: "Recent ",
-                          style: const TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black),
-                          children: [
-                            TextSpan(
-                                text: 'lists',
-                                style: TextStyle(
-                                    color: AppData.colors["buttonSelected"])),
-                          ]),
-                    ),
-                    height: MediaQuery.of(context).size.width *
-                        _scrollableCatHeight,
-                    onTap: (i) => print("recent item $i"),
-                    itemBuilder: (ctx, i) {
-                      return Container();
-                    })),
-            Container(
-                margin: const EdgeInsets.only(top: 40.0),
-                child: ScrollableCategory(
-                    label: RichText(
-                      text: TextSpan(
-                          text: "Place ",
-                          style: const TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black),
-                          children: [
-                            TextSpan(
-                                text: 'holder',
-                                style: TextStyle(
-                                    color: AppData.colors["buttonSelected"])),
-                          ]),
-                    ),
-                    height: MediaQuery.of(context).size.width *
-                        _scrollableCatHeight,
-                    onTap: (i) => print("place holder item $i"),
-                    itemBuilder: (ctx, i) {
-                      return Container();
-                    })),
-          ],
-        )),
-      ],
-    );
-  }
-}
-
-class ScrollableCategory extends StatefulWidget {
-  const ScrollableCategory({
-    Key? key,
-    this.label,
-    this.itemCount,
-    required this.onTap,
-    required this.height,
-    required this.itemBuilder,
-  }) : super(key: key);
-
-  final RichText? label;
-  final int? itemCount;
-  final double height;
-  final void Function(int) onTap;
-  final Widget Function(BuildContext, int) itemBuilder;
-
-  @override
-  State<ScrollableCategory> createState() => _ScrollableCategory();
-}
-
-class _ScrollableCategory extends State<ScrollableCategory> {
-  final double borderWidth = 2.0;
-
-  @override
-  Widget build(BuildContext ctx) {
-    return Container(
-      decoration: BoxDecoration(
-          border: Border.symmetric(
-              horizontal: BorderSide(
-                  color: AppData.colors["border"] ?? const Color(0xFF000000),
-                  width: borderWidth))),
-      child: Column(
-        children: [
-          widget.label == null
-              ? Container()
-              : Container(
-                  padding: const EdgeInsets.only(left: 10.0, top: 5.0),
-                  alignment: Alignment.topLeft,
-                  child: widget.label),
-          SizedBox(
-            height: widget.height,
-            child: ListView.builder(
-                //itemExtent: 1.0,
-                itemCount: widget.itemCount,
-                scrollDirection: Axis.horizontal,
-                //itemBuilder: widget.itemBuilder
-                itemBuilder: (BuildContext ctx, int i) {
-                  return GestureDetector(
-                      behavior: HitTestBehavior.deferToChild,
-                      onTap: () => widget.onTap(i),
-                      child: Container(
-                          margin: const EdgeInsets.all(10.0),
-                          width: widget.height - 10.0,
-                          decoration: BoxDecoration(
-                              color: AppData.colors["container"],
-                              border: Border.fromBorderSide(BorderSide(
-                                  color: AppData.colors["border"] ??
-                                      const Color(0xFF000000),
-                                  width: borderWidth,
-                                  style: BorderStyle.solid)),
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(5.0))),
-                          child: const Center(
-                            child: Text(""),
-                          )));
-                }),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ListTab extends StatefulWidget {
-  const ListTab({Key? key}) : super(key: key);
-
-  @override
-  State<ListTab> createState() => _ListTab();
-}
-
-class _ListTab extends State<ListTab> {
-  bool _enableMenus = true;
-  bool _refresh = false;
-
-  @override
-  Widget build(BuildContext ctx) {
-    return SimpleFileExplorer(
-      data: UserData.listData,
-      refresh: () {
-        bool ret = _refresh;
-        _refresh = false;
-        return ret;
-      },
-      enableMenus: () {
-        bool ret = _enableMenus;
-        _enableMenus = true;
-        return ret;
-      },
-      addBtns: [
-        FloatingActionButton(
-          onPressed: () => setState(() {
-            UserData.listData.add(AList(UserData.listData.wd, "myList"));
-            _enableMenus = false;
-            _refresh = true;
-          }),
-          child: const Icon(Icons.list),
-        )
-      ],
-    );
-  }
-}
-
-class QuizTab extends StatefulWidget {
-  const QuizTab({Key? key}) : super(key: key);
-
-  @override
-  State<QuizTab> createState() => _QuizTab();
-}
-
-class _QuizTab extends State<QuizTab> {
-  bool _enableMenus = true;
-
-  @override
-  Widget build(BuildContext ctx) {
-    return SimpleFileExplorer(
-      data: UserData.quizData,
-      enableMenus: () {
-        bool ret = _enableMenus;
-        _enableMenus = true;
-        return ret;
-      },
-      addBtns: [
-        FloatingActionButton(
-          onPressed: () => setState(() {
-            //UserData.quizData.add(AQuiz(UserData.listData.wd, "myQuiz"));
-            _enableMenus = false;
-          }),
-          child: const Icon(Icons.quiz),
-        )
+        Container(
+            margin: const EdgeInsets.all(20),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20))),
+            )),
       ],
     );
   }
