abb6fc04179a42bcaf5320457bf2c03300b91537
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-02-28 16:25:02 +0100

    Refactor filesystem

--- lib/data.dart
+++ lib/data.dart
@@ -1,460 +1,242 @@
-import 'dart:collection';
-import 'dart:math';
-import 'dart:io';
 import 'dart:convert';
-
-import 'package:tuple/tuple.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
-abstract class AFile {
-  AFile(this._parent, this.name);
+class AList {
+  AList(this.name)
+      : _entries = [],
+        _tags = {},
+        _stats = AListStats();
 
-  final dynamic _parent;
-  String name;
-  get parent => _parent;
-}
-
-class ACategory extends AFile {
-  ACategory(dynamic parent, String name)
-      : _table = {},
-        super(parent, name);
-
-  ACategory.fromJson(dynamic parent, String name, Map<String, dynamic> json)
-      : _table = Set.from(json['table']),
-        super(parent, name);
-
-  Map<String, dynamic> toJson() => {"table": _table.toList()};
-
-  final Set<int> _table;
-
-  bool contains(int id) {
-    return _table.contains(id);
-  }
-
-  bool add(int id) {
-    return _table.add(id);
-  }
-
-  bool delete(int id) {
-    return _table.remove(id);
-  }
-
-  Set getTable() {
-    return Set.from(_table);
-  }
-}
-
-class AList extends AFile {
-  AList(dynamic parent, String name) : super(parent, name);
   AList.from(AList list)
-      : _content = List.from(list._content),
-        super(list.parent, list.name);
+      : name = list.name,
+        path = list.path,
+        _entries = List.from(list._entries),
+        _tags = Set.from(list._tags),
+        _stats = AListStats();
 
-  AList.fromJson(dynamic parent, String name, Map<String, dynamic> json)
-      : _content = List.from(json["content"]),
-        super(parent, name);
+  AList._(this.name, List<Map> entries, Set<String> tags, this.path)
+      : _entries = List.from(entries),
+        _tags = Set.from(tags),
+        _stats = AListStats();
 
-  Map<String, dynamic> toJson() => {"content": _content};
+  factory AList.fromJson(String name, String rawData, String path) {
+    Map<String, dynamic> data = jsonDecode(rawData);
+    AList list = AList._(name, data['entries'], data['tags'], path);
 
-  List<Tuple2<String, String>> _content = [];
-
-  bool contains(Tuple2<String, String>? element) {
-    return _content.contains(element);
+    return list;
   }
 
-  bool containsAll(List elements) {
-    for (var e in elements) {
-      if (!_content.contains(e)) {
-        return false;
-      }
-    }
+  Map<String, dynamic> toJson() =>
+      {"entries": _entries, "tags": _tags, "stats": _stats.toJson()};
 
-    return true;
-  }
+  String name;
+  final List<Map> _entries;
+  final Set<String> _tags;
+  final AListStats _stats;
+  String path = '';
 
-  void add(Tuple2<String, String> item) {
-    _content.add(item);
-  }
-
-  void addAll(List<Tuple2<String, String>> items) {
-    _content.addAll(items);
-  }
-
-  Tuple2 delete(int index) {
-    return _content.removeAt(index);
-  }
+  int get length => _entries.length;
+  bool get isEmpty => _entries.isEmpty;
+  bool get isNotEmpty => _entries.isNotEmpty;
 }
 
-class AQuiz extends AFile {
-  AQuiz(dynamic parent, String name, this.lists, Map<String, dynamic> options)
-      : _options = options,
-        super(parent, name);
+class AListStats {
+  AListStats();
 
-  AQuiz.fromJson(dynamic parent, String name, Map<String, dynamic> json)
-      : lists = json["lists"],
-        _options = json["options"],
-        super(parent, name);
-
-  Map<String, dynamic> toJson() => {"lists": lists, "options": _options};
-
-  List lists;
-  final Map<String, dynamic> _options;
-
-  // Move to quiz widget
-  List<Tuple2<String, String>> mix(bool elt, bool lines,
-      {List<Tuple2<String, String>> selection = const []}) {
-    if (selection.isEmpty) {
-      selection = List.from(lists);
-    }
-
-    List _mix(List ls) {
-      List it = List.from(ls);
-      var dist = Random();
-      for (int i = 0; i < it.length; i++) {
-        int n = dist.nextInt(it.length);
-        var tmp = it[i];
-        it[i] = it[n];
-        it[n] = tmp;
-      }
-
-      return it;
-    }
-
-    if (elt) {
-      for (var e in selection) {
-        List tmp = _mix([e.item1, e.item2]);
-        e = Tuple2.fromList(tmp);
-      }
-    }
-
-    return selection;
-  }
+  Map<String, dynamic> toJson() => {"": ""};
 }
 
-enum DataType { category, list, quiz }
+//deals with mem
+class FileExplorer {
+  static final List<Directory> _loadedDir = [];
+  static final Map<String, int> _loadedLists = {};
+  static final List<AList> _loadedListsQueue = [];
+  static String _deepestPathOnCurrentBranch = '/';
+  static int _currDirIndex = 0;
+  static const int _loadedDirOffsetFromCurrent = 1;
+  static const _loadedListsQueueSize = 10;
+  static bool _isInit = false;
 
-class FileExplorerData {
-  ///if [filename] exist, data in it is used in initialization
-  FileExplorerData(String filename, Directory localDir)
-      : _itemsHeaders = SplayTreeMap(),
-        _idPtr = -1,
-        _headersFilename = localDir.path + filename,
-        _localDir = localDir {
-    _itemsHeaders = SplayTreeMap(compare, (a) => a > 0);
-    add(ACategory(null, 'root'));
-    FileExplorerData._loadData(this);
-
-    int? maxId = _itemsHeaders.lastKey();
-    if (maxId != null) _idPtr = FileExplorerData._removeTypeFromId(maxId);
-
-    assert(_idPtr > -2);
+  static init() async {
+    if (_isInit) return;
+    _isInit = true;
+    var dir = await getApplicationDocumentsDirectory();
+    createDirectory('${dir.path}/fe/root/', cd: true);
   }
 
-  SplayTreeMap<int, Tuple2<int, String>> _itemsHeaders;
-  final Map<int, AFile> _idTable = <int, AFile>{}; // loaded items
-  int _idPtr;
-  static final int _idTypeShift = 63 - DataType.values.last.index;
-  final String _headersFilename;
-  final List<int> navHistory = [];
-  final Directory _localDir;
-  int _wd = 0;
-  int get wd => _wd;
+  static String get current => Directory.current.path;
 
-  Future<bool> cd(int id) async {
-    bool ret = await _loadItem(id);
-    if (ret) _wd = id;
-    return ret;
+  static List<String> stripPath(String path) {
+    return (path.split('/'))..removeWhere((e) => e.isEmpty);
   }
 
-  /// Compare ids stripped of their type using 'compareTo' method
-  static int compare(int a, int b) {
-    return _removeTypeFromId(a).compareTo(_removeTypeFromId(b));
+  static bool _isDirLoaded(String path) {
+    return _loadedDir
+        .firstWhere((e) => e.path == path, orElse: () => Directory(''))
+        .path
+        .isNotEmpty;
   }
 
-  void clearHistory({int start = 0}) {
-    navHistory.removeRange(start, navHistory.length);
+  static void _updateDeepestPath(String newPath) {
+    if (!_deepestPathOnCurrentBranch.contains(newPath)) {
+      _deepestPathOnCurrentBranch = newPath;
+    }
   }
 
-  void clearCache() {
-    File jsonFile = File(_headersFilename);
-
-    deleteAll(_itemsHeaders.keys.toList());
-    jsonFile.deleteSync();
-    navHistory.clear();
+  static void _updateCurrentDirIndex(String path) {
+    assert(_deepestPathOnCurrentBranch.length >= path.length);
+    _currDirIndex = stripPath(path).length;
   }
 
-  static Map<String, dynamic> _headersToJson(
-      SplayTreeMap<int, Tuple2<int, String>> headers) {
-    Map<String, dynamic> ret = <String, dynamic>{};
+  static void _adjustLoadedDir() {
+    List dirs = stripPath(_deepestPathOnCurrentBranch);
+    List dirsToLoad = [];
 
-    headers.forEach((key, value) {
-      ret[key.toString()] = [value.item1.toString(), value.item2];
-    });
+    // get lists to be loaded
+    for (int i = _currDirIndex - _loadedDirOffsetFromCurrent;
+        i < _currDirIndex + _loadedDirOffsetFromCurrent;
+        ++i) {
+      dirsToLoad.add('/${dirs.sublist(0, i).join('/')}');
+    }
 
-    return ret;
+    // load lists if not already loaded
+    for (var e in _loadedDir) {
+      if (!dirsToLoad.contains(e.path) && dirsToLoad.isNotEmpty) {
+        e = Directory(dirsToLoad.removeLast());
+      }
+    }
   }
 
-  static SplayTreeMap<int, Tuple2<int, String>> _headersFromJson(
-      Map<String, dynamic> json) {
-    SplayTreeMap<int, Tuple2<int, String>> ret = SplayTreeMap(compare);
-
-    json.forEach((key, value) {
-      ret[int.parse(key)] = Tuple2(int.parse(value[0]), value[1]);
-    });
-
-    return ret;
+  static bool cd(String path) {
+    return _cdDir(Directory(path));
   }
 
-  /// Load [FileExplorerData] data if the a data file exists
-  static Future<bool> _loadData(FileExplorerData data) async {
-    File jsonFile = File(data._headersFilename);
+  static bool _cdDir(Directory dir) {
+    String path = dir.absolute.path;
 
-    if (!jsonFile.existsSync()) {
+    if (dir.existsSync()) {
+      if (!_isDirLoaded(path)) _loadedDir.add(dir);
+    } else {
       return false;
     }
 
-    //TODO: check file is valid
-    Map<String, dynamic> json = jsonDecode(jsonFile.readAsStringSync());
-    data._itemsHeaders.addAll(_headersFromJson(json));
-
-    // init root table
-    ACategory root = await data.get(0);
-
-    data._itemsHeaders.forEach((key, value) {
-      if (value.item1 == 0 && !root.getTable().contains(key)) root.add(key);
-    });
+    //may need to use absolute path
+    Directory.current = path;
+    _updateDeepestPath(path);
+    _updateCurrentDirIndex(path);
+    _adjustLoadedDir();
 
     return true;
   }
 
-  /// Save all headers in a single file
-  void _saveHeaders() {
-    File headerFile = File(_headersFilename);
-
-    headerFile.writeAsStringSync(
-        jsonEncode(FileExplorerData._headersToJson(_itemsHeaders)));
-  }
-
-  /// Save an item on the disk
-  void _saveItem(int id) async {
-    assert(id != 0, "Root must not be serialized");
-
-    File dataFile = File("${_localDir.path}/$id");
-    AFile? item = _idTable[id];
-
-    if (item != null) {
-      if (!_itemsHeaders.containsKey(id)) {
-        _itemsHeaders[id] = Tuple2(item.parent, item.name);
-        _saveHeaders();
-      }
-
-      dataFile.writeAsStringSync(jsonEncode(item));
+  static Type? getFileType(String path) {
+    dynamic ret;
+    if (Directory(path).existsSync()) {
+      ret = Directory;
+    } else if (File(path).existsSync()) {
+      ret = File;
     }
-  }
-
-  /// Delete an item from the disk if it exists
-  void _delete(int id) {
-    File file = File("${_localDir.path}/$id");
-
-    if (file.existsSync()) file.delete();
-  }
-
-  /// Load item from disk and add it to the id table
-  Future<bool> _loadItem(int id) async {
-    if (_idTable.containsKey(id)) return true;
-
-    File file = File("${_localDir.path}/$id");
-
-    if (!_itemsHeaders.containsKey(id) || !file.existsSync()) {
-      print('Cannot load $id');
-      return false;
-    }
-
-    String jsonString = file.readAsStringSync();
-    DataType dt = FileExplorerData.getTypeFromId(id);
-    AFile item;
-
-    // TODO: simplify/change this ugly ass bitch
-    switch (dt) {
-      case DataType.category:
-        item = ACategory.fromJson(_itemsHeaders[id]!.item1,
-            _itemsHeaders[id]!.item2, jsonDecode(jsonString));
-
-        break;
-      case DataType.list:
-        item = AList.fromJson(_itemsHeaders[id]!.item1,
-            _itemsHeaders[id]!.item2, jsonDecode(jsonString));
-
-        break;
-      default:
-        item = AQuiz.fromJson(_itemsHeaders[id]!.item1,
-            _itemsHeaders[id]!.item2, jsonDecode(jsonString));
-    }
-
-    _idTable[id] = item;
-
-    return true;
-  }
-
-  bool unloadItem(int id) {
-    //Don't remove root from id table !!!
-    return (id == 0) || _idTable.remove(id) != null ? true : false;
-  }
-
-  void unloadItems(List<int> ids) {
-    for (int id in ids) {
-      unloadItem(id);
-    }
-  }
-
-  /// Generate an id on 64 bits
-  /// The 'DataType.last.index' last bits are for the type
-  int _genId(DataType dt) {
-    int ret = ++_idPtr;
-
-    if (ret > pow(2, _idTypeShift)) {
-      throw Exception("Id must be $_idTypeShift bits -- $ret");
-    }
-
-    ret = (dt.index << _idTypeShift) | ret;
-
-    assert(!_itemsHeaders.containsKey(ret),
-        "$ret already assigned to ${_itemsHeaders[ret]!.item2}");
 
     return ret;
   }
 
-  static int _removeTypeFromId(int id) {
-    return (id & -1 << _idTypeShift) ^ id;
+  static List<String> listCurrentDir({SortType sortType = SortType.rct}) {
+    return listDir(Directory.current.path, sortType: sortType);
   }
 
-  static DataType getTypeFromId(int id) {
-    return DataType.values.firstWhere((e) => e.index == id >> _idTypeShift);
-  }
+  static List<String> listDir(String path, {SortType sortType = SortType.rct}) {
+    assert(_isInit);
 
-  /// Returns a set of invalid ids
-  Set checkIds(Set ids) {
-    return ids.difference(Set.from(_itemsHeaders.keys));
-  }
+    List<String> ret = [];
+    late Directory dir;
 
-  Future<int> add(AFile item) async {
-    // TODO: guess type outside class
-    DataType type = item is ACategory
-        ? DataType.category
-        : (item is AList ? DataType.list : DataType.quiz);
+    !_isDirLoaded(path)
+        ? dir = Directory(path)
+        : dir = _loadedDir.firstWhere((e) => e.path == path);
 
-    int id = _genId(type);
-    _idTable[id] = item;
+    if (dir.existsSync()) {
+      try {
+        ret = dir.listSync().map((e) {
+          return e.path;
+        }).toList();
 
-    if (id > 0) _saveItem(id);
-
-    await _updateParent(item.parent, id);
-
-    return id;
-  }
-
-  Future<void> _updateParent(int? pid, int id) async {
-    // ignore if root
-    if (pid != null) {
-      ACategory parent = await get(pid);
-      parent.add(id);
-
-      if (pid != 0) _saveItem(pid);
-    }
-  }
-
-  Future<bool> delete(int id) async {
-    if (id == 0) {
-      throw Exception("Root category must not be removed");
-    }
-
-    Tuple2<int, String>? ret = _itemsHeaders.remove(id);
-
-    // remove id from parent
-    if (ret != null) {
-      var tmp = _idTable.remove(id);
-
-      if (getTypeFromId(id) == DataType.category) {
-        ACategory? cat = tmp as ACategory? ?? await get(id);
-
-        if (cat != null) {
-          Set table = cat.getTable();
-          for (var e in table) {
-            await delete(e);
-          }
+        switch (sortType) {
+          case SortType.asc:
+            ret.sort((a, b) => a.compareTo(b));
+            break;
+          case SortType.dsc:
+            ret.sort((a, b) => b.compareTo(a));
+            break;
+          case SortType.rct:
+            //TODO: sort
+            break;
         }
+      } on FileSystemException catch (e) {
+        print(e.message);
+        print(Directory.current);
       }
-
-      ACategory parent = await get(ret.item1);
-      parent.delete(id);
-
-      if (navHistory.contains(id)) clearHistory(start: navHistory.indexOf(id));
-
-      ret.item1 != 0
-          ? _saveItem(ret.item1)
-          : _saveHeaders(); //_saveHeaders already called in _saveItem
-
-      _delete(id);
-
-      return true;
     }
 
-    return false;
+    return ret;
   }
 
-  void deleteAll(List<int> ids) {
-    for (int id in ids) {
-      delete(id);
+  static void _loadList(AList list) {
+    if (_loadedLists.length >= _loadedListsQueueSize &&
+        _loadedLists.isNotEmpty) {
+      _unloadLists();
     }
+
+    _loadedListsQueue.add(list);
   }
 
-  Future get(int id) async {
-    if (!_idTable.containsKey(id)) {
-      await _loadItem(id);
+  static void _unloadLists() {
+    _loadedListsQueue.removeAt(0);
+  }
+
+  //load a list
+  static AList? getList(String path) {
+    int i = _loadedListsQueue.indexWhere((e) => e.path == path);
+    AList? list = i >= 0 ? _loadedListsQueue[i] : null;
+
+    if (list == null) {
+      var tmp = File(path);
+
+      if (tmp.existsSync()) {
+        list = AList.fromJson(
+            stripPath(tmp.path).last, tmp.readAsStringSync(), path);
+        _loadList(list);
+      }
     }
-    return _idTable[id];
+
+    return list;
   }
 
-  String? getName(int id) {
-    return (id == 0) ? 'Root' : _itemsHeaders[id]?.item2;
+  void writeList(String path) {
+    if (_loadedLists[path] == null) return;
+
+    File file = File(path);
+    AList list = _loadedListsQueue[_loadedLists[path]!];
+    file.writeAsStringSync(jsonEncode(list.toJson()));
   }
 
-  int? getParent(int id) {
-    return _itemsHeaders[id]?.item1;
+  static String createList(AList list, {String dirPath = ''}) {
+    File file = File('$dirPath${list.name}');
+    file.createSync();
+    list.path = file.absolute.path;
+
+    return file.path;
+  }
+
+  static void createDirectory(String path, {bool cd = false}) {
+    Directory dir = Directory(path);
+    dir.createSync(recursive: true);
+    if (cd) _cdDir(dir);
   }
 }
 
-class UserData {
-  static FileExplorerData? _listData;
-  static FileExplorerData? _quizData;
-
-  static bool _init = false;
-
-  static get listData => _check(_listData);
-  static get quizData => _check(_quizData);
-
-  static _check(dynamic value) {
-    if (!_init) {
-      throw Exception("UserData must be initialized before it can be used");
-    }
-
-    return value;
-  }
-
-  static Future<void> init() async {
-    if (_init) return;
-
-    final localPath = await _getLocalDir();
-    _init = true;
-    _listData = FileExplorerData("list_data", localPath);
-    _quizData = FileExplorerData("quiz_data", localPath);
-  }
-
-  static Future<Directory> _getLocalDir() async {
-    return await getApplicationDocumentsDirectory();
-  }
-}
+enum SortType { rct, asc, dsc }
 
 class AppData {
   static Map<String, Color> colors = {
@@ -467,6 +249,23 @@ class AppData {
   };
 }
 
+class ATab {
+  ATab(
+      {required this.icon,
+      required Widget child,
+      bool isMain = false,
+      this.onWillPop})
+      : tabIcon = isMain ? const Icon(Icons.home) : icon,
+        tab = child,
+        bMain = isMain;
+
+  final Icon icon;
+  Icon tabIcon;
+  Widget tab;
+  bool bMain;
+  bool Function()? onWillPop;
+}
+
 Future<void> loadInitialData() async {
-  await UserData.init();
+  await FileExplorer.init();
 }
