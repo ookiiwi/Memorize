f96e0cfe55c187669b992a312bd1106dd25a54a4
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-03-16 19:02:21 +0100

    Implement default quiz page widget for jpn

--- lib/addon.dart
+++ lib/addon.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:memorize/parser.dart';
+import 'package:memorize/tab.dart';
+
+Map<String, Addon> addons = {'JpnAddon': JpnAddon()};
 
 abstract class Addon {
+  String mode = 'Default';
+  List get modes;
+
   Widget buildListEntryPreview(Map entry);
   Widget buildListEntryPage(Map entry);
+  Widget buildQuizPage(List<Map> entries, {bool showWord = true});
 }
 
 class GraphicAddon extends Addon {
+  @override
+  List get modes => [];
+
   @override
   Widget buildListEntryPreview(Map entry) {
     return RecursiveDescentParser.parse('a-(b|c)',
@@ -17,13 +27,22 @@ class GraphicAddon extends Addon {
   Widget buildListEntryPage(Map entry) {
     return Container();
   }
+
+  @override
+  Widget buildQuizPage(List<Map> entries, {bool showWord = true}) {
+    return Container();
+  }
 }
 
 class JpnAddon extends Addon {
-  final EdgeInsets _margin =
-      const EdgeInsets.symmetric(horizontal: 5, vertical: 2.5);
+  static const EdgeInsets _margin =
+      EdgeInsets.symmetric(horizontal: 5, vertical: 2.5);
 
-  Widget _buildPreviewSecondaryField({required String text, Color? color}) {
+  @override
+  List get modes => ['Default'];
+
+  static Widget _buildPreviewSecondaryField(
+      {required String text, Color? color}) {
     return Container(
       padding: const EdgeInsets.all(5),
       margin: _margin,
@@ -33,14 +52,14 @@ class JpnAddon extends Addon {
     );
   }
 
-  List<Widget> _buildPreviewSecondaryFields(
+  static List<Widget> _buildPreviewSecondaryFields(
       {required List texts, Color? color}) {
     return texts
         .map((e) => _buildPreviewSecondaryField(text: e, color: color))
         .toList();
   }
 
-  List<Widget> _buildMeanings(Map entry) {
+  static List<Widget> _buildMeanings(Map entry) {
     List<Widget> ret = [];
 
     for (var e in entry['meanings']) {
@@ -110,10 +129,8 @@ class JpnAddon extends Addon {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
-            height: 50 * scaleFactor,
-            width: 50 * scaleFactor,
             margin: const EdgeInsets.all(10),
-            //padding: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(20),
             decoration: BoxDecoration(
                 color: Colors.black26, borderRadius: BorderRadius.circular(20)),
             child: Center(
@@ -130,5 +147,117 @@ class JpnAddon extends Addon {
         child: Text(entry["meanings"].join(", ")),
       )
     ]);
+  }
+
+  @override
+  Widget buildQuizPage(List<Map> entries, {bool showWord = true}) {
+    return JpnAddonDefaultMode(entries: entries);
+  }
+}
+
+class JpnAddonDefaultMode extends StatefulWidget {
+  const JpnAddonDefaultMode({Key? key, required this.entries})
+      : super(key: key);
+  final List<Map> entries;
+
+  @override
+  State<JpnAddonDefaultMode> createState() => _JpnAddonDefaultMode();
+}
+
+class _JpnAddonDefaultMode extends State<JpnAddonDefaultMode> {
+  late final List<Map> entries;
+  PageController pageController = PageController(keepPage: false);
+  final RouteController routeController =
+      RouteController(canPop: () async => true);
+
+  bool _showAnswer = false;
+
+  @override
+  void initState() {
+    super.initState();
+    entries = widget.entries;
+
+    pageController.addListener(() {
+      double page = pageController.page ?? 0;
+      if (page.floor() > entries.length) {
+        Future.delayed(const Duration(microseconds: 1),
+            () => setState(() => pageController.jumpToPage(0)));
+      }
+    });
+  }
+
+  Widget _buildQuizEntry(Map entry, bool showAnswer) {
+    return Center(
+        child: Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.amber, borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  entry.containsKey("word") ? entry["word"] : entry["kanji"],
+                  textScaleFactor: 6,
+                ),
+                if (showAnswer) ...[
+                  Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: JpnAddon._buildMeanings(entry))
+                ]
+              ],
+            )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        PageView.builder(
+            onPageChanged: ((value) {
+              if (value >= entries.length - 1 && !_showAnswer) {
+                _showAnswer = true;
+              }
+            }),
+            controller: pageController,
+            itemCount: entries.length + (_showAnswer ? 0 : 2),
+            itemBuilder: (context, i) {
+              if (i == entries.length) {
+                return const Center(
+                  child: Text('Ad'),
+                );
+              }
+              return _buildQuizEntry(
+                  entries[
+                      (i + (i > entries.length ? -1 : 0)) % (entries.length)],
+                  _showAnswer);
+            }),
+        Container(
+            margin: const EdgeInsets.all(20),
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                    margin: const EdgeInsets.all(10),
+                    child: FloatingActionButton(
+                        onPressed: () {
+                          pageController.previousPage(
+                              duration: const Duration(milliseconds: 450),
+                              curve: Curves.linear);
+                        },
+                        child: const Icon(Icons.arrow_left_rounded))),
+                Container(
+                    margin: const EdgeInsets.all(10),
+                    child: FloatingActionButton(
+                        onPressed: () {
+                          pageController.nextPage(
+                              duration: const Duration(milliseconds: 450),
+                              curve: Curves.linear);
+                        },
+                        child: const Icon(Icons.arrow_right_rounded))),
+              ],
+            ))
+      ],
+    );
   }
 }
