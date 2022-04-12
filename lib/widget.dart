42c391e3e1668435b8c1a9d6063ffc98ad3b54e9
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-04-12 19:32:15 +0200

    Implement search and test swipe widget

--- lib/widget.dart
+++ lib/widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:matrix_gesture_detector/matrix_gesture_detector.dart';
+import 'package:vector_math/vector_math_64.dart' as vec;
 
 class Selectable extends StatefulWidget {
   const Selectable(
@@ -231,5 +233,209 @@ class _ExpandedWidget extends State<ExpandedWidget>
       axisAlignment: 1.0,
       child: widget.child,
     );
+  }
+}
+
+class Swipe extends StatefulWidget {
+  const Swipe({Key? key, required this.child}) : super(key: key);
+
+  final Widget child;
+  @override
+  State<Swipe> createState() => _Swipe();
+}
+
+class _Swipe extends State<Swipe> {
+  Matrix4 _matrix = Matrix4.identity();
+
+  @override
+  Widget build(BuildContext context) {
+    return MatrixGestureDetector(
+        shouldRotate: false,
+        shouldScale: false,
+        shouldTranslate: true,
+        onMatrixUpdate: (m, tm, sm, rm) {
+          tm[13] = 0.0;
+          print(tm);
+          setState(() =>
+              _matrix = MatrixGestureDetector.compose(_matrix, tm, null, null));
+        },
+        child: Transform(transform: _matrix, child: widget.child));
+  }
+}
+
+class SearchWidget extends StatefulWidget {
+  const SearchWidget(
+      {Key? key, this.height, this.width, this.builder, this.fetchData})
+      : super(key: key);
+  final double? width;
+  final double? height;
+  final Widget Function(BuildContext, dynamic data)? builder;
+  final Future<List> Function(String)? fetchData;
+  @override
+  State<SearchWidget> createState() => _SearchWidget();
+}
+
+class _SearchWidget extends State<SearchWidget> {
+  final TextEditingController _controller = TextEditingController();
+  final GlobalKey _key = GlobalKey();
+  late final OverlayEntry _overlay;
+  bool _overlayOpen = false;
+  List _searchData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _overlay = _overlayEntry();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    //overlay setState
+    //_overlay.markNeedsBuild();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _manageOverlay() {
+    if (widget.builder != null) {
+      _controller.text.isNotEmpty
+          ? widget.fetchData!(_controller.text).then((value) {
+              _searchData = value;
+              _showOverlay(context);
+            })
+          : _hideOverlay();
+    }
+  }
+
+  void _showOverlay(BuildContext context) {
+    if (!_overlayOpen) {
+      OverlayState? state = Overlay.of(context);
+      if (state != null && _searchData.isNotEmpty) {
+        state.insert(_overlay);
+        _overlayOpen = true;
+      }
+    } else {
+      _overlay.markNeedsBuild();
+    }
+  }
+
+  void _hideOverlay() {
+    if (_overlayOpen) _overlay.remove();
+    _overlayOpen = false;
+  }
+
+  Widget _overlayBody() {
+    final RenderBox renderBox =
+        _key.currentContext!.findRenderObject() as RenderBox;
+
+    final Size size = renderBox.size;
+    final Offset position = renderBox.localToGlobal(Offset.zero);
+    double topPos = position.dy + size.height + 10;
+
+    return Positioned(
+        top: topPos,
+        left: position.dx,
+        child: Material(
+            child: LimitedBox(
+          maxWidth: size.width,
+          maxHeight: MediaQuery.of(context).size.height * 0.9 - (topPos),
+          child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.lightBlue,
+              ),
+              child: ListView.builder(
+                  itemCount: _searchData.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, i) {
+                    return Container(
+                        margin: EdgeInsets.only(
+                            top: (i > 0 ? 5 : 0),
+                            bottom: i < _searchData.length - 1 ? 5 : 0),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: ElevatedButton(
+                            style: ButtonStyle(
+                                overlayColor: MaterialStateProperty.resolveWith(
+                                    (states) => Colors.amber),
+                                shape: MaterialStateProperty.resolveWith(
+                                    (states) => RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(20)))),
+                            onPressed: () {},
+                            child: widget.builder!(context, _searchData[i])));
+                  })),
+        )));
+  }
+
+  OverlayEntry _overlayEntry() {
+    return OverlayEntry(builder: (context) {
+      return _key.currentContext != null
+          ? _overlayBody()
+          : Positioned(
+              height: 500,
+              width: 100,
+              child: Material(
+                  child: Container(
+                height: 500,
+                width: 100,
+                color: Colors.red,
+              )));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        key: _key,
+        height: widget.height,
+        width: widget.width,
+        child: Column(children: [
+          Expanded(
+              child: Container(
+                  padding: const EdgeInsets.only(
+                      left: 20, top: 0, bottom: 0, right: 5),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(30)),
+                  child: Row(
+                    children: [
+                      Container(
+                          margin: const EdgeInsets.all(5),
+                          child:
+                              GestureDetector(child: const Icon(Icons.search))),
+                      Expanded(
+                          child: Container(
+                              padding: const EdgeInsets.all(10),
+                              child: TextField(
+                                enableSuggestions: false,
+                                controller: _controller,
+                                onChanged: (value) {
+                                  //setState(() {});
+                                  _manageOverlay();
+                                },
+                                decoration: InputDecoration.collapsed(
+                                    hintText: 'Search...',
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey.shade400)),
+                              ))),
+                      //if (_controller.text.isNotEmpty)
+                      Container(
+                          margin: const EdgeInsets.all(5),
+                          child: GestureDetector(
+                              onTap: () {
+                                setState(() => _controller.clear());
+                                _manageOverlay();
+                              },
+                              child: const Icon(Icons.cancel))),
+                    ],
+                  )))
+        ]));
   }
 }
