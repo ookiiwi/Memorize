968f6ef432272ed2b95e3c0e6a2a7f674f2a2d0a
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-04-19 12:06:20 +0200

    Refactor file explorer

--- lib/data.dart
+++ lib/data.dart
@@ -2,10 +2,13 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:memorize/auth.dart';
 import 'package:memorize/reminder.dart';
+import 'package:memorize/web/login.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
 import 'package:nanoid/nanoid.dart';
+import 'package:http/http.dart' as http;
 
 int daysBetween(DateTime from, DateTime to) {
   from = DateTime(from.year, from.month, from.day);
@@ -46,7 +49,7 @@ class AList {
     AList list = AList._(List.from(data['entries']), Set.from(data['tags']),
         path, AListStats.fromJson(data["listStats"]));
 
-    ReminderNotification.add(list._reminder);
+    //ReminderNotification.add(list._reminder);
     return list;
   }
 
@@ -67,7 +70,7 @@ class AList {
 
   AListStats get stats => _stats;
 
-  String get uniqueName => "$_id?$name";
+  String get uniqueName => name; //"$_id?$name";
 
   String get path => "$_dirPath$uniqueName";
   set path(String p) => _readPath(p);
@@ -163,199 +166,6 @@ List<String> stripPath(String path) {
   return (path.split('/'))..removeWhere((e) => e.isEmpty);
 }
 
-//deals with mem
-abstract class FileExplorer {
-  static final List _loadedDirs = [];
-  static final List<AList> _loadedListsQueue = [];
-  static String _deepestPathOnCurrentBranch = '/';
-  static int _currDirIndex = 0;
-  static const int _loadedDirsOffsetFromCurrent = 1;
-  static const _loadedListsQueueSize = 10;
-  static bool _isInit = false;
-  static late FileExplorer _fe;
-  static late String _rootPath;
-  static Uuid uuid = const Uuid();
-
-  static dynamic init(FileExplorer fe) async {
-    if (_isInit) return;
-    _isInit = true;
-    _fe = fe;
-    await fe._initRootPath();
-    createDirectory(_rootPath, cd: true);
-  }
-
-  dynamic _initRootPath();
-
-  static MapEntry<String, String> _splitName(String name) {
-    String id = name.replaceAll(RegExp(r"\?.+"), '');
-    String rname = name.replaceFirst(id + '?', '');
-    return MapEntry(id, rname);
-  }
-
-  static String get current => _fe._current;
-  static String get currentRelative => toRelative(current);
-  String _toRelative(String path);
-  static String toRelative(String path) => _fe._toRelative(path);
-
-  set _current(String path);
-  String get _current;
-
-  bool _exists(String path);
-  static bool exists(String path) => _fe._exists(path);
-
-  bool _isDirLoaded(String path);
-
-  void _unloadDir(String path);
-
-  static void _updateDeepestPath(String newPath) {
-    if (!_deepestPathOnCurrentBranch.contains(newPath)) {
-      _deepestPathOnCurrentBranch = newPath;
-    }
-  }
-
-  static void _updateCurrentDirIndex(String path) {
-    assert(_deepestPathOnCurrentBranch.length >= path.length);
-    _currDirIndex = stripPath(path).length;
-  }
-
-  void _loadDirs(List dirsToLoad);
-
-  //static
-  static void _adjustLoadedDir() {
-    List dirs = stripPath(_deepestPathOnCurrentBranch);
-    List dirsToLoad = [];
-
-    // get lists to be loaded
-    for (int i = _currDirIndex - _loadedDirsOffsetFromCurrent;
-        i < _currDirIndex + _loadedDirsOffsetFromCurrent;
-        ++i) {
-      dirsToLoad.add('/${dirs.sublist(0, i).join('/')}');
-    }
-
-    _fe._loadDirs(dirsToLoad);
-
-    assert(_loadedDirs.length <= 2 * _loadedDirsOffsetFromCurrent + 1);
-  }
-
-  static bool cd(String path) {
-    return _fe._cdDir(Directory(path));
-  }
-
-  bool _cdDir(dynamic dir);
-
-  void _sanityCheck(String path) {
-    //may need to use absolute path
-    _current = path; //+ call current setter from FE
-    _updateDeepestPath(path);
-    _updateCurrentDirIndex(path);
-    _adjustLoadedDir();
-  }
-
-  static bool isDirectory(String path) => _fe._isDirectory(path);
-  bool _isDirectory(String path);
-
-  static List<String> listCurrentDir({SortType sortType = SortType.rct}) {
-    return _fe.listDir(_fe._current, sortType: sortType);
-  }
-
-  List<String> _listDir(String path, {SortType sortType = SortType.rct});
-  List<String> listDir(String path, {SortType sortType = SortType.rct}) {
-    assert(_isInit);
-    return _fe._listDir(path, sortType: sortType);
-  }
-
-  static void _loadList(AList list) {
-    if (_loadedListsQueue.length >= _loadedListsQueueSize) _popList();
-
-    _loadedListsQueue.add(list);
-  }
-
-  static void _unloadList(String path) {
-    _loadedListsQueue.removeWhere((e) => e.path == path);
-  }
-
-  static void _popList() {
-    _loadedListsQueue.removeAt(0);
-  }
-
-  //load a list
-  static AList? getList(String path, {void Function()? ifNoList}) {
-    int i = _loadedListsQueue.indexWhere((e) => e.path == path);
-    AList? list = i >= 0 ? _loadedListsQueue[i] : null;
-
-    if (list == null) {
-      var rawList = _fe._loadRawListFromDisk(path);
-
-      if (rawList != null) {
-        var splittedName = _splitName(stripPath(path).last);
-        list =
-            AList.fromJson(splittedName.key, splittedName.value, rawList, path);
-        _loadList(list);
-      }
-    }
-
-    if (ifNoList != null && list == null) {
-      ifNoList();
-    }
-
-    return list;
-  }
-
-  static void _updateList(AList list) {
-    int i = _loadedListsQueue.indexWhere((e) => e.path == list.path);
-    if (i >= 0) {
-      _loadedListsQueue[i] = list;
-    }
-  }
-
-  String? _loadRawListFromDisk(String path);
-
-  static bool _isListLoaded(String path) =>
-      _loadedListsQueue.any((e) => e.path == path);
-
-  void _writeList(AList list);
-  static void writeListFromPath(String path) {
-    if (!FileExplorer._isListLoaded(path)) {
-      throw Exception('List not loaded, can\'t write it');
-    }
-
-    AList? list = getList(path);
-    if (list != null) {
-      writeList(list);
-    }
-  }
-
-  static void writeList(AList list) {
-    if (!FileExplorer._isListLoaded(list.path)) {
-      throw Exception('List not loaded, can\'t write it');
-    }
-    _updateList(list);
-
-    _fe._writeList(list);
-  }
-
-  void _createList(AList list, {String dirPath = ''});
-  static void createList(AList list, {String dirPath = ''}) =>
-      _fe._createList(list, dirPath: dirPath);
-
-  static bool canRenameList(AList list, String newName, {bool rename = true}) =>
-      _fe._canRenameList(list, newName);
-  bool _canRenameList(AList list, String newName, {bool rename = true});
-
-  static void createDirectory(String path, {bool cd = false}) {
-    var dir = _fe._createDirectory(path);
-    if (cd) _fe._cdDir(dir);
-  }
-
-  dynamic _createDirectory(String path);
-
-  static void clearDir(String path) => _fe._clearDir(path);
-  void _clearDir(String path);
-
-  static void delete(String path) => _fe._delete(path);
-  void _delete(String path);
-}
-
 enum SortType { rct, asc, dsc }
 
 class AppData {
@@ -389,244 +199,18 @@ class AppBarItem {
   final bool Function()? onWillPop;
 }
 
-class MobileFileExplorer extends FileExplorer {
-  @override
-  set _current(String path) => Directory.current = path;
-
-  @override
-  String get _current => Directory.current.path;
-
-  @override
-  bool _exists(String path) {
-    if (Directory(path).existsSync()) return true;
-    if (File(path).existsSync()) return true;
-    return false;
-  }
-
-  @override
-  String _toRelative(String path) =>
-      '/' + path.replaceFirst(RegExp('.+/root(/|)'), '');
-
-  @override
-  dynamic _initRootPath() async {
-    var dir = await getApplicationDocumentsDirectory();
-    FileExplorer._rootPath = '${dir.path}/fe/root/';
-  }
-
-  @override
-  bool _isDirectory(String path) => Directory(path).existsSync() ? true : false;
-
-  @override
-  bool _isDirLoaded(String path) {
-    return FileExplorer._loadedDirs
-        .firstWhere((e) => e is Directory && e.path == path,
-            orElse: () => Directory(''))
-        .path
-        .isNotEmpty;
-  }
-
-  @override
-  void _unloadDir(String path) {
-    FileExplorer._loadedDirs
-        .removeWhere((e) => e is Directory && e.path == path);
-  }
-
-  @override
-  void _loadDirs(List dirsToLoad) {
-    for (var e in FileExplorer._loadedDirs) {
-      if (!dirsToLoad.contains(e.path) && dirsToLoad.isNotEmpty) {
-        e = Directory(dirsToLoad.removeLast());
-      }
-    }
-  }
-
-  @override
-  bool _cdDir(dynamic dir) {
-    String path = dir.path;
-
-    if (!dir.existsSync() ||
-        (Directory.current.path.endsWith('/root') &&
-            !path.contains(Directory.current.path))) {
-      return false;
-    }
-    if (!_isDirLoaded(path)) FileExplorer._loadedDirs.add(dir);
-
-    _sanityCheck(path);
-
-    return true;
-  }
-
-  @override
-  List<String> _listDir(String path, {SortType sortType = SortType.rct}) {
-    List<String> ret = [];
-    late Directory dir;
-    !_isDirLoaded(path)
-        ? dir = Directory(path)
-        : dir = FileExplorer._loadedDirs.firstWhere((e) => e.path == path);
-    if (dir.existsSync()) {
-      try {
-        ret = dir.listSync().map((e) {
-          return e.path;
-        }).toList();
-        switch (sortType) {
-          case SortType.asc:
-            ret.sort((a, b) => a.compareTo(b));
-            break;
-          case SortType.dsc:
-            ret.sort((a, b) => b.compareTo(a));
-            break;
-          case SortType.rct:
-            //TODO: sort
-            break;
-        }
-      } on FileSystemException catch (e) {
-        stderr.write(e.message);
-        stderr.write(Directory.current);
-      }
-    }
-    return ret;
-  }
-
-  @override
-  String? _loadRawListFromDisk(String path) {
-    var tmp = File(path);
-    if (tmp.existsSync()) {
-      return tmp.readAsStringSync();
-    }
-
-    return null;
-  }
-
-  @override
-  void _createList(AList list, {String dirPath = ''}) {
-    File file = File('$dirPath${list.uniqueName}');
-
-    list.path = file.absolute.path;
-    FileExplorer._loadList(list);
-  }
-
-  @override
-  void _writeList(AList list) {
-    File file = File(list.path);
-    if (!file.existsSync()) {
-      file.createSync();
-    }
-
-    //we suppose the list is already load
-    file.writeAsStringSync(jsonEncode(list.toJson()));
-  }
-
-  @override
-  bool _canRenameList(AList list, String newName, {bool rename = true}) {
-    String oldName = list.name;
-    bool ret = true;
-    list.name = newName;
-
-    File file = File(list.path);
-    ret = (newName == oldName) || !file.existsSync();
-    if (!rename || !ret) list.name = oldName;
-    return true;
-  }
-
-  @override
-  dynamic _createDirectory(String path) =>
-      Directory(path)..createSync(recursive: true);
-
-  @override
-  void _clearDir(String path) {
-    var dir = Directory(path);
-    print('delete path: $path');
-    if (dir.existsSync()) {
-      dir.listSync().forEach((e) => e.deleteSync());
-    }
-  }
-
-  @override
-  void _delete(String path) {
-    var item = _isDirectory(path) ? Directory(path) : File(path);
-
-    if (item is File && FileExplorer._isListLoaded(path)) {
-      FileExplorer._unloadList(path);
-    } else if (_isDirLoaded(path)) {
-      _unloadDir(path);
-    }
-
-    item.delete(recursive: true);
-  }
-}
-
-class CloudFileExplorer extends FileExplorer {
-  String _curr = '';
-
-  @override
-  set _current(String path) => _curr = path;
-
-  @override
-  String get _current => _curr;
-
-  @override
-  bool _exists(String path) => true;
-
-  @override
-  String _toRelative(String path) => _curr;
-
-  @override
-  dynamic _initRootPath() async {}
-
-  @override
-  bool _isDirectory(String path) {
-    return true;
-  }
-
-  @override
-  bool _isDirLoaded(String path) {
-    return true;
-  }
-
-  @override
-  void _unloadDir(String path) {}
-
-  @override
-  void _loadDirs(List dirsToLoad) {}
-
-  @override
-  bool _cdDir(dynamic dir) {
-    return true;
-  }
-
-  @override
-  String? _loadRawListFromDisk(String path) {}
-
-  @override
-  List<String> _listDir(String path, {SortType sortType = SortType.rct}) {
-    return [];
-  }
-
-  void _createList(AList list, {String dirPath = ''}) {}
-
-  @override
-  void _writeList(AList list) {
-    if (FileExplorer._isListLoaded(list.path)) return;
-  }
-
-  bool _canRenameList(AList list, String newName, {bool rename = true}) {
-    return true;
-  }
-
-  @override
-  dynamic _createDirectory(String path, {bool cd = false}) {}
-  void _clearDir(String path) {}
-
-  void _delete(String path) {}
-}
-
 class DataLoader {
   static bool _isDataLoaded = false;
 
   static load({bool force = false}) async {
+    print('r: $_isDataLoaded');
     if (_isDataLoaded && !force) return;
-    await FileExplorer.init(
-        kIsWeb ? CloudFileExplorer() : MobileFileExplorer());
+    print('rr');
+    currentUser = await UserInfo.retrieve();
+    print('curr: $currentUser');
+    //await FileExplorer.init(
+    //    kIsWeb ? CloudFileExplorer() : MobileFileExplorer());
+    print('rrr');
     if (!kIsWeb) await ReminderNotification.init();
     _isDataLoaded = true;
     //await Future.delayed(const Duration(seconds: 2));
---
+++ lib/file_explorer.dart
@@ -0,0 +1,331 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:memorize/auth.dart';
+import 'package:memorize/data.dart';
+import 'package:memorize/web/login.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+enum FileType { dir, list, unknown }
+
+class FileInfo {
+  FileInfo(this.type, this.name);
+  FileInfo.guess(String type, this.name) {
+    print("type: $type");
+    if (type == 'dir' || type == 'directory') {
+      this.type = FileType.dir;
+    } else if (type == 'file') {
+      this.type = FileType.list;
+    } else {
+      this.type = FileType.unknown;
+    }
+  }
+  late final FileType type;
+  final String name;
+}
+
+abstract class FileExplorer {
+  String get wd;
+
+  dynamic fetch(String listname);
+  dynamic write(AList list);
+  dynamic move(String src, String dest);
+  dynamic remove(String filename);
+  Future<List<FileInfo>> ls({String dir = '.'});
+  dynamic cd(String path);
+  dynamic mkdir(String path);
+}
+
+class CloudFileExplorer extends FileExplorer {
+  static const _serverUrl = "http://192.168.1.12";
+  static const _root = '/fe';
+  String _wd = _root;
+
+  @override
+  String get wd => _wd.replaceFirst(_root, '');
+
+  void _check() {
+    assert(
+        currentUser != null &&
+            currentUser!.status == UserConnectionStatus.loggedIn,
+        'User must be logged in order to save lists.');
+  }
+
+  String _absolutePath(String path) {
+    if (path.startsWith(RegExp(r"\.$|\./"))) {
+      path = path.replaceFirst(RegExp(r"\."), _wd);
+    } else if (path.startsWith(RegExp(r"(\.\.)$|(\.\./)"))) {
+      if (_wd != _root) {
+        String rpDir =
+            _wd.split('/').reversed.skip(1).toList().reversed.join('/');
+        path = path.replaceFirst(RegExp(r"\.\."), rpDir);
+      } else {
+        path = _wd;
+      }
+    } else if (!path.startsWith(_root)) {
+      path = _root + (path.startsWith('/') ? '' : '/') + path;
+    }
+
+    return path;
+  }
+
+  @override
+  dynamic fetch(String listname) async {
+    _check();
+    AList? ret;
+
+    try {
+      var response =
+          await http.post(Uri.parse("$_serverUrl/front_end_api/data.php"),
+              body: jsonEncode({
+                'action': 'get',
+                'username': currentUser!.username,
+                'pwd': currentUser!.password,
+                'filename': "$_root/$listname"
+              }));
+
+      if (response.statusCode == 200) {
+        ret = AList.fromJson(
+            '', listname.split('/').last, response.body, listname);
+      } else {
+        print('notfound');
+      }
+    } catch (e) {
+      print('fetch error: $e');
+    }
+    return ret;
+  }
+
+  @override
+  dynamic write(AList list) async {
+    _check();
+
+    try {
+      var response = await http.post(
+          Uri.parse("$_serverUrl/front_end_api/data.php?dbg=true"),
+          body: jsonEncode({
+            'action': 'upload',
+            'username': currentUser!.username,
+            'pwd': currentUser!.password,
+            'filename': "$_wd/${list.path}",
+            'data': jsonEncode(list.toJson())
+          }));
+
+      return response.body;
+    } catch (e) {
+      print('write error: $e');
+    }
+
+    return null;
+  }
+
+  @override
+  dynamic move(String src, String dest) {}
+  @override
+  dynamic remove(String filename) async {
+    try {
+      var response = await http.post(
+          Uri.parse("$_serverUrl/front_end_api/data.php?dbg=true"),
+          body: jsonEncode({
+            'action': 'rm',
+            'username': currentUser!.username,
+            'pwd': currentUser!.password,
+            'filename': _absolutePath(filename)
+          }),
+          encoding: Encoding.getByName('utf-8'));
+
+      return response.body;
+    } catch (e) {
+      print('ls error: $e');
+    }
+
+    return null;
+  }
+
+  @override
+  Future<List<FileInfo>> ls({String dir = '.'}) async {
+    print("ls ${_absolutePath(dir)}");
+    try {
+      var response = await http.post(
+          Uri.parse("$_serverUrl/front_end_api/data.php?dbg=true"),
+          body: jsonEncode({
+            'action': 'ls',
+            'username': currentUser!.username,
+            'pwd': currentUser!.password,
+            'filename': _absolutePath(dir)
+          }),
+          encoding: Encoding.getByName('utf-8'));
+      return List.from(jsonDecode(response.body))
+          .map((e) => FileInfo.guess(e["type"], e["name"]))
+          .toList();
+    } catch (e) {
+      print('ls error: $e');
+    }
+
+    return [];
+  }
+
+  @override
+  dynamic cd(String path) {
+    String absPath = _absolutePath(path);
+    int i = -1;
+    List rootCodeUnits = _root.codeUnits;
+    List pathCodeUnits = absPath.codeUnits;
+
+    if (absPath.length < _root.length) {
+      print('error cd');
+      return;
+    }
+
+    while (++i < rootCodeUnits.length) {
+      if (rootCodeUnits[i] != pathCodeUnits[i]) {
+        return;
+      }
+    }
+
+    _wd = absPath;
+  }
+
+  @override
+  dynamic mkdir(String path) async {
+    try {
+      var response = await http.post(
+          Uri.parse("$_serverUrl/front_end_api/data.php?dbg=true"),
+          body: jsonEncode({
+            'action': 'mkdir',
+            'username': currentUser!.username,
+            'pwd': currentUser!.password,
+            'filename': _absolutePath(path)
+          }),
+          encoding: Encoding.getByName('utf-8'));
+      //print('response : ${response.body}');
+    } catch (e) {
+      print('ls error: $e');
+    }
+  }
+}
+
+class MobileFileExplorer extends FileExplorer {
+  MobileFileExplorer() {
+    _fRoot = getApplicationDocumentsDirectory()
+      ..then((value) {
+        Directory fe = Directory("${value.absolute.path}/fe");
+        if (!fe.existsSync()) {
+          fe.createSync();
+          print('create fe dir');
+        }
+        Directory.current = fe;
+        _root = Directory.current;
+      });
+  }
+
+  late final Future<Directory> _fRoot;
+  Directory? _root;
+  String _wd = '';
+
+  @override
+  String get wd => _wd;
+
+  String? _feAbsolutePath(String path) {
+    if (!path.startsWith(_root!.absolute.path)) {
+      return null;
+    }
+
+    return path.replaceFirst(_root!.absolute.path, '');
+  }
+
+  Future _check() async {
+    if (_root == null) {
+      await _fRoot;
+    }
+
+    assert(_root != null);
+  }
+
+  @override
+  dynamic fetch(String listname) async {
+    await _check();
+    File file = File("./$listname");
+
+    if (file.existsSync()) {
+      return AList.fromJson(
+          '', listname.split('/').last, file.readAsStringSync(), listname);
+    }
+
+    return null;
+  }
+
+  @override
+  dynamic write(AList list) async {
+    _check();
+
+    //TODO: use listname not name
+    File file = File("${_root!.absolute.path}/${list.name}");
+
+    if (!file.existsSync()) {
+      file.createSync();
+    }
+
+    file.writeAsStringSync(jsonEncode(list.toJson()));
+  }
+
+  @override
+  dynamic move(String src, String dest) {}
+
+  @override
+  dynamic remove(String filename) {
+    final Directory dir = Directory(filename);
+    final File file = File(filename);
+
+    if (dir.existsSync()) {
+      dir.deleteSync(recursive: true);
+    } else if (file.existsSync()) {
+      file.deleteSync();
+    }
+  }
+
+  @override
+  Future<List<FileInfo>> ls({String dir = '/'}) async {
+    await _check();
+
+    Directory _dir = Directory(".$dir");
+    List<FileInfo> ret = [];
+
+    if (_dir.existsSync()) {
+      _dir.listSync().forEach((e) => ret.add(FileInfo.guess(
+          e.statSync().type.toString(), e.path.split('/').last)));
+    }
+
+    return ret;
+  }
+
+  @override
+  dynamic cd(String path) async {
+    await _check();
+    final Directory dir = Directory(path);
+    final String resPath = Platform.script.resolve(dir.absolute.path).path;
+
+    if (!dir.existsSync()) {
+      print('error cd: dir does not exists');
+      return;
+    }
+
+    String? absp = _feAbsolutePath(resPath);
+
+    if (absp != null) {
+      _wd = absp.endsWith('/') ? absp.substring(0, absp.length - 1) : absp;
+      Directory.current = resPath;
+    }
+  }
+
+  @override
+  dynamic mkdir(String path) async {
+    await _check();
+
+    Directory dir = Directory(path);
+
+    dir.createSync(recursive: true);
+  }
+}
