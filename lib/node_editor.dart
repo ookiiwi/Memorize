507aea9eed7059adfe0a70ab631490f9e2d8915c
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-07-07 14:05:50 +0200

    Implement node focused on top

--- lib/node_editor.dart
+++ lib/node_editor.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:memorize/data.dart';
 import 'package:memorize/node.dart';
 import 'package:memorize/widget.dart';
+import 'package:provider/provider.dart';
 
 class NodeEditor extends StatefulWidget with ATab {
   const NodeEditor({Key? key}) : super(key: key);
@@ -21,14 +22,16 @@ class _NodeEditor extends State<NodeEditor> {
   double dWidth = 0;
   final TransformationController _controller = TransformationController();
   late final NodeController _nodeController;
+  final NodeLinkController _nodeLinkController = NodeLinkController();
   final List<Node> _nodes = [];
-  Widget? outputWidget;
+  ValueNotifier<Widget?> outputWidget = ValueNotifier(null);
+  final ValueNotifier<Offset?> linkNotifier = ValueNotifier(null);
 
   @override
   void initState() {
     super.initState();
     document.onContextMenu.listen((event) => event.preventDefault());
-    _nodeController = NodeController();
+    _nodeController = NodeController(toScene: _controller.toScene);
 
     _nodes.addAll([
       InputNodeGroup(
@@ -43,13 +46,19 @@ class _NodeEditor extends State<NodeEditor> {
         controller: _nodeController,
         offset: const Offset(800, 200),
         dataCallback: (data) {
-          setState(() {
-            //print('got data');
-            outputWidget = data;
-          });
+          outputWidget.value = data;
         },
-      )
+      ),
     ]);
+
+    _nodeController.focusedNode.addListener(() {
+      int i =
+          _nodes.indexWhere((e) => e.id == _nodeController.focusedNode.value);
+
+      if (i < 0 || _nodes.isEmpty) return;
+
+      _nodes.add(_nodes.removeAt(i));
+    });
   }
 
   @override
@@ -69,51 +78,72 @@ class _NodeEditor extends State<NodeEditor> {
 
   @override
   Widget build(BuildContext context) {
-    return Listener(
-        onPointerDown: (event) {
-          if (event.kind == PointerDeviceKind.mouse &&
-              event.buttons == kSecondaryMouseButton) {
-            final Offset pos = event.position;
+    return ContextMenuManager(
+        builder: (context, child) => Listener(
+            onPointerDown: (event) {
+              if (event.kind == PointerDeviceKind.mouse &&
+                  event.buttons == kSecondaryMouseButton) {
+                final Offset pos = event.position;
 
-            showContextMenu(
-                context,
-                RelativeRect.fromLTRB(
-                    pos.dx, pos.dy, pos.dx + 100, pos.dy + 150),
-                [
-                  ContextMenuItem(
-                      onTap: () {
-                        _addNodes([
-                          ContainerNode(
-                              offset: _controller.toScene(pos),
-                              controller: _nodeController)
-                        ]);
-                      },
-                      child: const Text('Container'))
-                ]);
-          }
-        },
-        child: Row(children: [
-          SizedBox(
-              height: dHeight,
-              width: 1000,
-              child: InteractiveViewer(
-                  transformationController: _controller,
-                  constrained: false,
-                  child: Container(
-                    height: dHeight,
-                    width: dWidth,
-                    color: Colors.transparent,
-                    child: Stack(
-                      children: [
-                        ..._nodes
-                            .map(
-                              (e) => e,
-                            )
-                            .toList()
-                      ],
-                    ),
-                  ))),
-          if (outputWidget != null) Expanded(child: outputWidget!)
-        ]));
+                linkNotifier.value = _controller.toScene(event.localPosition);
+
+                showContextMenu(
+                    context,
+                    RelativeRect.fromLTRB(
+                        pos.dx, pos.dy, pos.dx + 100, pos.dy + 150),
+                    [
+                      ContextMenuItem(
+                          onTap: () {
+                            _addNodes([
+                              ContainerNode(
+                                  key: UniqueKey(),
+                                  offset: _controller.toScene(pos),
+                                  controller: _nodeController)
+                            ]);
+                          },
+                          child: const Text('Container'))
+                    ]);
+              } else {
+                linkNotifier.value = null;
+              }
+            },
+            child: Row(children: [
+              SizedBox(
+                  height: dHeight,
+                  width: 1000,
+                  child: InteractiveViewer(
+                      transformationController: _controller,
+                      constrained: false,
+                      child: Container(
+                        height: dHeight,
+                        width: dWidth,
+                        color: Colors.transparent,
+                        child: ValueListenableBuilder(
+                            valueListenable: _nodeController.linksLayer,
+                            builder:
+                                (context, List<NodeLink> linksLayer, child) {
+                              return RepaintBoundary(
+                                  child: CustomPaint(
+                                      painter: NodeLinkPainter(
+                                          context, linkNotifier,
+                                          links: linksLayer,
+                                          nodeLinkController:
+                                              _nodeLinkController),
+                                      child: ValueListenableBuilder(
+                                          valueListenable:
+                                              _nodeController.focusedNode,
+                                          builder: (context, value, child) {
+                                            return Stack(children: _nodes);
+                                          })));
+                            }),
+                      ))),
+              ValueListenableBuilder(
+                  valueListenable: outputWidget,
+                  builder: (context, cnt, child) {
+                    return outputWidget.value != null
+                        ? Expanded(child: outputWidget.value!)
+                        : Container();
+                  })
+            ])));
   }
 }
