04feb1f4d8a07446d174d98990210c74c3879fc9
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-08-12 12:03:33 +0200

    Implement root node to manage nodes lifecycle

--- lib/node.dart
+++ lib/node.dart
@@ -1,477 +1,193 @@
+import 'package:directed_graph/directed_graph.dart';
 import 'package:flutter/material.dart';
-import 'package:memorize/widget.dart';
-import 'package:multi_value_listenable_builder/multi_value_listenable_builder.dart';
-import 'package:nanoid/nanoid.dart';
-import 'package:provider/provider.dart';
+import 'package:memorize/node_base.dart';
 
 enum IOType { input, output, none }
 
-abstract class Node {
-  Node();
-  Node.fromJson(Map<String, dynamic> json)
-      : properties =
-            List.from(json["properties"].map((e) => Property.fromJson(e)));
+typedef DataEmissionApprover = bool Function(Node node);
 
-  Map<String, dynamic> toJson() =>
-      {"properties": properties.map((e) => e.toJson())};
+class NodeUtil {
+  static Node fromJson(Map<String, dynamic> json) {
+    switch (json['runtimeType']) {
+      case 'ContainerNode':
+        return ContainerNode.fromJson(json);
+      case 'InputGroup':
+        return InputGroup.fromJson(json);
+      case 'OutputGroup':
+        return OutputGroup.fromJson(json);
+      default:
+        throw TypeError();
+    }
+  }
 
-  late final List<Property> properties;
+  static IOType ioTypeFromString(String type) =>
+      IOType.values.firstWhere((e) => e.toString() == type);
 }
 
-class Property {
-  Property(this.type,
-      {this.data, this.builderName, this.builderOptions = const []})
-      : connections = [];
-
-  Property.fromJson(Map<String, dynamic> json, {this.data})
-      : type = json["type"],
-        connections = List.from(json["connections"]),
-        builderName = json["builderName"],
-        builderOptions = json["builderOptions"];
-
-  Map<String, dynamic> toJson() => {
-        "type": type,
-        "connections": connections,
-        "builderName": builderName,
-        "builderOptions": builderOptions
-      };
-
-  final List<String> connections;
-  final String? builderName;
-  final List builderOptions;
-  ValueNotifier? data;
-  IOType type;
-}
-
-class VisualNode extends StatefulWidget {
-  const VisualNode(
-      {Key? key, required this.node, this.onDelete, this.offset = Offset.zero})
-      : super(key: key);
-
-  @override
-  State<VisualNode> createState() => _VisualNode();
+class NodeConnDetails {
+  NodeConnDetails(this.node, this.port);
 
   final Node node;
-  final bool Function()? onDelete;
-  final Offset offset;
+  final int port;
+  String get connId => '${node.id}.$port';
 }
 
-class _VisualNode extends State<VisualNode> {
-  late final Matrix4 _matrix;
-  final _offset = ValueNotifier(Offset.zero);
+class RootNode {
+  RootNode() : graph = DirectedGraph({}, comparator: _comparator);
 
-  TapDownDetails? _rightClickDetails;
-  late final List<Layer> layers;
+  RootNode.fromJson(Map<String, dynamic> json)
+      : graph = DirectedGraph({}, comparator: _comparator) {
+    final Map<String, dynamic> nodes = Map.from(json['nodes']);
+    final Map<String, List> graphOfIds = Map.from(json['graph']);
 
-  @override
-  void initState() {
-    super.initState();
+    for (MapEntry<String, List> entry in graphOfIds.entries) {
+      if (entry.value.isEmpty) continue;
 
-    _matrix = Matrix4.identity()..translate(widget.offset.dx, widget.offset.dy);
-    _offset.value = widget.offset;
-    layers = Provider.of(context, listen: false) ?? [];
-  }
+      final node = NodeUtil.fromJson(nodes[entry.key]);
 
-  @override
-  Widget build(BuildContext context) {
-    return Transform(
-        transform: _matrix,
-        child: GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onSecondaryTapDown: (details) => _rightClickDetails = details,
-            onSecondaryTap: () {
-              if (_rightClickDetails == null) return;
+      for (int i = 0; i < node.outputProps.length; ++i) {
+        for (var port in node.outputProps[i].connections) {
+          final nodeId = splitId(port)[0];
+          final propId = splitId(port)[1];
 
-              final pos = _rightClickDetails!.globalPosition;
-
-              // call user callback instead
-              showContextMenu(
-                  context,
-                  RelativeRect.fromLTRB(
-                      pos.dx, pos.dy, pos.dx + 100, pos.dy + 150),
-                  [
-                    ContextMenuItem(
-                        onTap: () {
-                          Navigator.of(context).pop();
-
-                          if (widget.onDelete == null || widget.onDelete!()) {
-                            //TODO: set offset to infinite == deletion
-                            _offset.value = Offset.infinite;
-                            layers[1].remove(widget);
-                          }
-                        },
-                        child: const Text('Delete'))
-                  ]);
-            },
-            onPanUpdate: (details) {
-              setState(() {
-                _matrix.translate(details.delta.dx, details.delta.dy);
-                _offset.value =
-                    _offset.value.translate(details.delta.dx, details.delta.dy);
-              });
-            },
-            child: Provider.value(
-                updateShouldNotify: (_, __) => false,
-                value: _offset,
-                builder: (context, child) => Container(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    decoration: BoxDecoration(
-                        color: Colors.blueGrey,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: IntrinsicWidth(
-                        child:
-                            Column(mainAxisSize: MainAxisSize.min, children: [
-                      Container(
-                          margin: const EdgeInsets.all(5),
-                          child: Text(
-                              '${widget.node.runtimeType}${widget.node.hashCode}'
-                                  .toString()
-                                  .replaceFirst('Node', ''))),
-                      ...widget.node.properties
-                          .map((e) => VisualProperty(property: e))
-                          .toList(),
-                    ]))))));
-  }
-}
-
-class VisualProperty extends StatefulWidget {
-  const VisualProperty({Key? key, required this.property}) : super(key: key);
-
-  final Property property;
-
-  @override
-  State<VisualProperty> createState() => _VisualProperty();
-}
-
-class _VisualProperty extends State<VisualProperty> {
-  get options => widget.property.builderOptions;
-
-  Widget get child {
-    assert(
-        widget.property.type == IOType.output && widget.property.data != null ||
-            widget.property.type == IOType.input);
-
-    Widget ret;
-
-    switch (widget.property.builderName) {
-      case "slider":
-        ret = buildSlider();
-        break;
-      default:
-        return const SizedBox();
+          connect(
+              NodeConnDetails(node, i),
+              Set.from(entry.value.map((e) =>
+                  NodeConnDetails(NodeUtil.fromJson(nodes[nodeId]), propId))));
+        }
+      }
     }
+  }
 
+  Map<String, dynamic> toJson() {
+    final Map nodes = {};
+    final Map graphOfIds = graph.data.map((key, value) {
+      nodes[key.id] = key.toJson();
+      return MapEntry(
+          key.id,
+          value.map((e) {
+            return e.id;
+          }).toList());
+    });
+
+    return {'nodes': nodes, 'graph': graphOfIds};
+  }
+
+  final DirectedGraph<Node> graph;
+  int _idPtr = 0;
+  int get newId => _idPtr++;
+
+  static int _comparator(Node a, Node b) => a.id.compareTo(b.id);
+  static String makeId(String nodeId, String port) => '$nodeId.$port';
+
+  static List splitId(String id) {
+    final List ret = List.from(id.split('.'));
+    ret.add(int.parse(ret.removeLast()));
     return ret;
   }
 
-  /// min
-  /// max
-  Widget buildSlider() {
-    return ValueListenableBuilder(
-        valueListenable: widget.property.data!,
-        builder: (context, value, child) => Slider(
-            min: options[0],
-            max: options[1],
-            value: widget.property.data!.value,
-            onChanged: (value) => widget.property.data!.value = value));
-  }
+  void connect(NodeConnDetails output, Set<NodeConnDetails> inputs) {
+    graph.addEdges(output.node, inputs.map((e) => e.node).toSet());
 
-  @override
-  Widget build(BuildContext context) {
-    return Provider.value(
-        value: widget.property,
-        child: Row(
-          mainAxisAlignment: widget.property.type != IOType.output
-              ? MainAxisAlignment.start
-              : MainAxisAlignment.end,
-          children: [
-            if (widget.property.type == IOType.input) const NodeIO(),
-            ConstrainedBox(
-                constraints: const BoxConstraints(minHeight: 50), child: child),
-            if (widget.property.type == IOType.output) const NodeIO()
-          ],
-        ));
-  }
-}
+    final cycles = graph.cycle;
 
-class NodeIO extends StatefulWidget {
-  const NodeIO({Key? key, this.dimension = 10, this.dummy = false})
-      : super(key: key);
-
-  final double dimension;
-  final bool dummy;
-
-  @override
-  State<NodeIO> createState() => _NodeIO();
-}
-
-class _NodeIO extends State<NodeIO> {
-  late final List<Layer> layers;
-  late final Offset Function(Offset) toScene;
-  late final Property property;
-  late final ValueNotifier<Offset> _nodeOffset;
-  Offset _posdiff = Offset.zero;
-
-  ValueNotifier<Offset>? _offset;
-  final double dimension = 10;
-  final _anchorKey = GlobalKey();
-  final anchor = ValueNotifier(Offset.zero);
-  ValueNotifier<Offset>? _linksPrevEnd;
-
-  Link? get _lastLink {
-    if (layers.isNotEmpty) {
-      if (layers.first.isNotEmpty) {
-        return layers.first.last;
-      }
+    for (var input in inputs) {
+      output.node.connect(output.port, input.node.inputProps[input.port],
+          _canEmitData(output.node), _checkCycle(cycles, input.node));
     }
-    return null;
   }
 
-  @override
-  void initState() {
-    super.initState();
+  bool _checkCycle(List cycles, Node node) =>
+      cycles.isNotEmpty && cycles.contains(node);
 
-    layers = Provider.of(context, listen: false) ?? [];
-    toScene = Provider.of(context, listen: false) ?? (off) => off;
-    property = Provider.of(context, listen: false);
-    _nodeOffset = Provider.of(context, listen: false)..addListener(_nodeMoved);
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      anchor.value = _computeAnchor;
-      _posdiff = _nodeOffset.value;
-    });
+  void disconnect(NodeConnDetails output, Set<NodeConnDetails> inputs) {
+    graph.removeEdges(
+        output.node,
+        inputs.map((e) {
+          output.node.disconnect(output.port, e.node.inputProps[e.port]);
+          return e.node;
+        }).toSet());
   }
 
-  @override
-  void dispose() {
-    _nodeOffset.removeListener(_nodeMoved);
-    super.dispose();
-  }
+  void addNode(Node node) => graph.addEdges(node, {});
+  void removeNode(Node node) => graph.remove(node);
 
-  void _nodeMoved() {
-    Offset off = _nodeOffset.value;
-    if (off == Offset.infinite) {
-      layers.first.removeWhere((e) {
-        if (property.connections.contains(e.id)) {
-          e.connState = ConnectionState.none;
-          return true;
-        }
-        return false;
-      });
-      return;
-    }
-
-    anchor.value += off - _posdiff;
-    _posdiff = off;
-  }
-
-  Offset get _computeAnchor {
-    var pos = getWidgetPosition(_anchorKey);
-    assert(pos != null);
-    Offset ret = Offset(pos!.dx + dimension / 2, pos.dy + dimension / 2);
-    return toScene(ret);
-  }
-
-  @override
-  void didUpdateWidget(oldWidget) {
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return widget.dummy
-        ? SizedBox.square(
-            dimension: widget.dimension,
-          )
-        : MouseRegion(
-            onEnter: (event) {
-              //might be a connection attemp
-
-              if (_offset != null ||
-                  !event.down ||
-                  _lastLink == null ||
-                  !Link.isOnHold(_lastLink!.id)) return;
-              _linksPrevEnd = _lastLink?.end;
-              _lastLink?.end = anchor;
-
-              if (property.type == IOType.output) {
-                _lastLink!.data = property.data;
-              }
-
-              property.connections.add(_lastLink!.id);
-              _lastLink!.connState = ConnectionState.active;
-            },
-            onExit: (event) {
-              //no conn or conn fixed
-
-              if (event.down && _linksPrevEnd != null) {
-                property.connections.remove(_lastLink!.id);
-                _lastLink?.end = _linksPrevEnd!;
-                _lastLink?.connState = ConnectionState.waiting;
-              }
-              _linksPrevEnd = null;
-            },
-            child: GestureDetector(
-                behavior: HitTestBehavior.translucent,
-                onPanStart: (details) {
-                  anchor.value = _computeAnchor;
-                  _offset = ValueNotifier(toScene(details.globalPosition));
-
-                  layers[0].insert(Link(anchor, _offset!,
-                      data: property.type == IOType.output
-                          ? property.data
-                          : null));
-                },
-                onPanUpdate: (details) {
-                  _offset!.value = toScene(details.globalPosition);
-                },
-                onPanEnd: (details) {
-                  _offset = null;
-                  Link.isOnHold(_lastLink!.id)
-                      ? layers.first.removeLast()
-                      : property.connections.add(_lastLink!.id);
-                },
-                child: Container(
-                  key: _anchorKey,
-                  height: widget.dimension,
-                  width: widget.dimension,
-                  decoration: const BoxDecoration(
-                      shape: BoxShape.circle, color: Colors.grey),
-                )));
-  }
-}
-
-class Link extends StatelessWidget {
-  Link(ValueNotifier<Offset> start, ValueNotifier<Offset> end,
-      {Key? key,
-      this.color,
-      this.stroke,
-      this.data,
-      ConnectionState connState = ConnectionState.waiting})
-      : id = nanoid(),
-        _start = ValueNotifier(start),
-        _end = ValueNotifier(end),
-        connStateNotifier = ValueNotifier(connState),
-        super(key: key) {
-    connStateNotifier.addListener(_manageConnState);
-    _manageConnState();
-  }
-
-  final String id;
-  final ValueNotifier<ValueNotifier<Offset>> _start;
-  ValueNotifier<Offset> get start => _start.value;
-  set start(ValueNotifier<Offset> notifier) => _start.value = notifier;
-
-  final ValueNotifier<ValueNotifier<Offset>> _end;
-  ValueNotifier<Offset> get end => _end.value;
-  set end(ValueNotifier<Offset> notifier) => _end.value = notifier;
-  final Color? color;
-  final double? stroke;
-
-  final ValueNotifier<ConnectionState> connStateNotifier;
-  ConnectionState get connState => connStateNotifier.value;
-  set connState(ConnectionState state) => connStateNotifier.value = state;
-
-  dynamic data;
-
-  static final Set<String> _connectionsOnHold = {};
-  static bool isOnHold(String id) => _connectionsOnHold.contains(id);
-
-  void _manageConnState() {
-    connState == ConnectionState.waiting
-        ? _connectionsOnHold.add(id)
-        : _connectionsOnHold.remove(id);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return RepaintBoundary(
-        child: MultiValueListenableBuilder(
-            valueListenables: [_start, _end],
-            builder: (context, value, child) => CustomPaint(
-                painter:
-                    LinkPainter(start, end, color: color, stroke: stroke))));
-  }
-}
-
-class LinkPainter extends CustomPainter {
-  LinkPainter(this.start, this.end, {Color? color, double? stroke})
-      : color = color ?? Colors.red,
-        stroke = stroke ?? 4.0,
-        super(repaint: Listenable.merge([start, end]));
-
-  final ValueNotifier<Offset> start;
-  final ValueNotifier<Offset> end;
-  final Color color;
-  final double stroke;
-  Path _path = Path();
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
-      ..color = color
-      ..strokeWidth = stroke;
-
-    Offset a = start.value - Offset(0, stroke / 2);
-    Offset b = end.value - Offset(0, stroke / 2);
-    Offset n = Offset(-(b.dy - a.dy), b.dx - a.dx);
-    Offset u = n / n.distance * stroke;
-    Offset c = b + u;
-    Offset d = a + u;
-
-    _path = Path()
-      ..moveTo(a.dx, a.dy)
-      ..lineTo(b.dx, b.dy)
-      ..lineTo(c.dx, c.dy)
-      ..lineTo(d.dx, d.dy)
-      ..close();
-
-    canvas.drawPath(_path, paint);
-  }
-
-  @override
-  bool shouldRepaint(LinkPainter oldDelegate) {
-    return oldDelegate.start.value != start.value ||
-        oldDelegate.end.value != end.value;
+  bool _canEmitData(Node node) {
+    final start = graph.firstWhere((e) => e is InputGroup, orElse: () => node);
+    return graph.path(start, node).isNotEmpty;
   }
 }
 
 class ContainerNode extends Node {
   ContainerNode() : _argb = List.generate(4, (i) => ValueNotifier(255.0)) {
-    _constructProps();
+    outputProps = List.unmodifiable([
+      OutputProperty('$id.0', data: wrapData(() => null, _argb)),
+    ]);
+
+    inputProps = List.unmodifiable(List.generate(
+        _argb.length,
+        (i) => InputProperty('$id.$i',
+            builderName: "slider", builderOptions: [0, 255], data: _argb[i])));
   }
 
   ContainerNode.fromJson(Map<String, dynamic> json)
       : _argb = json["argb"].map((e) => ValueNotifier(e)).toList(),
         super.fromJson(json) {
-    _constructProps();
+    outputProps = List.unmodifiable(
+        [OutputProperty.fromJson(json, data: wrapData(() => null, _argb))]);
+
+    inputProps = List.unmodifiable(List.generate(
+        _argb.length, (i) => InputProperty.fromJson(json, data: _argb[i])));
   }
 
+  @override
   Map<String, dynamic> toJson() =>
       super.toJson()..addAll({"argb": _argb.map((e) => e.value)});
 
-  final outputData = ValueNotifier<Widget?>(null);
-  final List _argb;
-
-  void _constructProps() {
-    properties = [
-      Property(IOType.output, data: outputData),
-      ...List.generate(
-          _argb.length,
-          (i) => Property(IOType.input,
-              builderName: "slider", builderOptions: [0, 255], data: _argb[i])),
-    ];
-  }
+  final List<ValueNotifier> _argb;
 }
 
 class InputGroup extends Node {
   InputGroup() {
-    properties = [Property(IOType.output, data: ValueNotifier(null))];
+    outputProps = List.unmodifiable([
+      OutputProperty(
+        '$id.0',
+      )
+    ]);
+
+    inputProps = List.unmodifiable([]);
+  }
+
+  InputGroup.fromJson(Map<String, dynamic> json) : super.fromJson(json) {
+    outputProps = List.unmodifiable(
+        json["outputProps"].map((e) => OutputProperty.fromJson(e)));
+    inputProps = List.unmodifiable([]);
   }
 }
 
 class OutputGroup extends Node {
   OutputGroup() {
-    properties = [Property(IOType.input)];
+    outputProps = List.unmodifiable([]);
+    inputProps = List.unmodifiable([InputProperty('$id.0')]);
   }
+
+  OutputGroup.fromJson(Map<String, dynamic> json) : super.fromJson(json) {
+    outputProps = List.unmodifiable([]);
+    inputProps = List.unmodifiable(
+        json['inputProps'].map((e) => InputProperty.fromJson(e)));
+  }
+}
+
+class DummyNode extends Node {
+  DummyNode() {
+    outputProps = [
+      OutputProperty('$id.0',
+          data: wrapData(() => DateTime.now(), [inputData])),
+    ];
+    inputProps = [
+      InputProperty('$id.0', data: inputData),
+    ];
+  }
+
+  final inputData = ValueNotifier<dynamic>(null);
 }
