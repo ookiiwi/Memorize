271c974b00b93aabddc3fbc24e56804f8cc803d4
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-08-16 14:59:17 +0200

    Clean tests and add test for data transmission after loop cut

--- test/node_test.dart
+++ test/node_test.dart
@@ -2,17 +2,21 @@ import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:memorize/node.dart';
+import 'package:memorize/node_base.dart';
 
 void main() {
   group('connection', () {
-    final RootNode root = RootNode();
+    late final RootNode root;
     final input = InputGroup();
     final output = OutputGroup();
 
+    setUpAll(() => root = RootNode());
+    tearDownAll(() => getIt.resetScope(dispose: false));
+
     test('connection is successful', () {
       expect(
-          () => root
-              .connect(NodeConnDetails(input, 0), {NodeConnDetails(output, 0)}),
+          () =>
+              root.connect(input.outputProps.first, {output.inputProps.first}),
           returnsNormally);
     });
 
@@ -22,17 +26,33 @@ void main() {
   });
 
   group('data transmission', () {
+    tearDown(() => getIt.resetScope(dispose: false));
+
     test('data pass from input grp to output grp', () {
       final root = RootNode();
       final input = InputGroup();
       final dummy = DummyNode();
       final output = OutputGroup();
 
-      root.connect(NodeConnDetails(input, 0), {NodeConnDetails(dummy, 0)});
-      root.connect(NodeConnDetails(dummy, 0), {NodeConnDetails(output, 0)});
+      root.connect(input.outputProps.first, {dummy.inputProps.first});
+      root.connect(dummy.outputProps.first, {output.inputProps.first});
 
       expect(output.inputProps[0].data, isNotNull);
       expect(output.inputProps[0].data, equals(dummy.outputProps[0].data));
+    });
+
+    test('data pass', () {
+      final root = RootNode();
+      final input = InputGroup();
+      final dummy = DummyNode();
+      final dummy2 = DummyNode();
+      final output = OutputGroup();
+
+      root.connect(input.outputProps.first, {dummy.inputProps.first});
+      root.connect(dummy.outputProps.first, {dummy2.inputProps.first});
+      root.connect(dummy2.outputProps.first, {output.inputProps.first});
+
+      expect(output.inputProps[0].data, isNotNull);
     });
 
     test('data do not pass from orphan node to output grp', () {
@@ -40,10 +60,40 @@ void main() {
       final dummy = DummyNode();
       final output = OutputGroup();
 
-      root.connect(NodeConnDetails(dummy, 0), {NodeConnDetails(output, 0)});
+      root.connect(dummy.outputProps.first, {output.inputProps.first});
       dummy.inputProps[0].data = DateTime.now();
 
       expect(output.inputProps[0].data, isNull);
+    });
+
+    test('data set after input grp is connected', () {
+      final root = RootNode();
+      final input = InputGroup();
+      final dummy = DummyNode();
+      final output = OutputGroup();
+
+      root.connect(dummy.outputProps.first, {output.inputProps.first});
+      expect(output.inputProps[0].data, isNull);
+
+      root.connect(input.outputProps.first, {dummy.inputProps.first});
+      expect(output.inputProps[0].data, isNotNull);
+    });
+
+    test('data set after input grp connected and no output gpr in graph', () {
+      final root = RootNode();
+      final input = InputGroup();
+      final dummy = DummyNode();
+      final dummy2 = DummyNode();
+
+      root.connect(dummy.outputProps.first, {dummy2.inputProps.first});
+      expect(dummy2.inputProps[0].data, isNull);
+
+      root.connect(input.outputProps.first, {dummy.inputProps.first});
+      expect(dummy2.inputProps[0].data, isNotNull);
+    });
+
+    test('data emitted on input changed', () {
+      // TODO: test
     });
   });
 
@@ -64,20 +114,24 @@ void main() {
   });
 
   group('Root serialization', () {
-    RootNode root = RootNode();
+    final input = InputGroup();
+    final output = OutputGroup();
+    String json = "";
+
+    tearDown(() => getIt.resetScope(dispose: false));
 
     test('toJson', () {
-      final input = InputGroup();
-      final output = OutputGroup();
-      root.connect(NodeConnDetails(input, 0), {NodeConnDetails(output, 0)});
-      expect(() => jsonEncode(root), returnsNormally);
+      final root = RootNode();
+      root.connect(input.outputProps.first, {output.inputProps.first});
+      expect(() => json = jsonEncode(root), returnsNormally);
     });
 
     test('fromJson', () {
-      final json = jsonDecode(jsonEncode(root));
+      late final RootNode root;
+      final jsonData = jsonDecode(json);
 
       expect(() {
-        root = RootNode.fromJson(json);
+        root = RootNode.fromJson(jsonData);
       }, returnsNormally);
 
       final input = root.graph.firstWhere((e) => e is InputGroup);
@@ -89,16 +143,17 @@ void main() {
   });
 
   group('disconnection', () {
-    RootNode root = RootNode();
-    InputGroup input = InputGroup();
-    OutputGroup output = OutputGroup();
+    tearDown(() => getIt.resetScope(dispose: false));
 
     test('disconnection is successful', () {
-      root.connect(NodeConnDetails(input, 0), {NodeConnDetails(output, 0)});
+      RootNode root = RootNode();
+      InputGroup input = InputGroup();
+      OutputGroup output = OutputGroup();
+      root.connect(input.outputProps.first, {output.inputProps.first});
 
       expect(
-          () => root.disconnect(
-              NodeConnDetails(input, 0), {NodeConnDetails(output, 0)}),
+          () => root
+              .disconnect(input.outputProps.first, {output.inputProps.first}),
           returnsNormally);
 
       expect(output.inputProps[0].data, isNull);
@@ -106,46 +161,68 @@ void main() {
   });
 
   group('cycle', () {
-    final root = RootNode();
+    tearDown(() => getIt.resetScope(dispose: false));
 
     test('self to self', () {
+      final root = RootNode();
       final container = ContainerNode();
-      root.connect(
-          NodeConnDetails(container, 0), {NodeConnDetails(container, 1)});
 
-      expect(container.outputProps[0].cycles.contains('${container.id}.1'),
+      root.connect(container.outputProps.first, {container.inputProps.first});
+
+      expect(container.outputProps[0].cycles.contains('${container.id}.0'),
           equals(true));
-      root.disconnect(
-          NodeConnDetails(container, 0), {NodeConnDetails(container, 1)});
     });
 
-    final container = ContainerNode();
-    final container2 = ContainerNode();
-
     test('cyclic only graph', () {
-      root.connect(
-          NodeConnDetails(container, 0), {NodeConnDetails(container2, 1)});
+      final root = RootNode();
+      final container = DummyNode();
+      final container2 = DummyNode();
 
-      root.connect(
-          NodeConnDetails(container2, 0), {NodeConnDetails(container, 1)});
+      root.connect(container.outputProps.first, {container2.inputProps.first});
+      root.connect(container2.outputProps.first, {container.inputProps.first});
 
       expect(container.outputProps[0].cycles.isEmpty, equals(true));
-      expect(container2.outputProps[0].cycles.contains('${container.id}.1'),
+      expect(container2.outputProps[0].cycles.contains('${container.id}.0'),
           equals(true));
     });
 
     test('cycle in graph', () {
+      final root = RootNode();
       final input = InputGroup();
       final output = OutputGroup();
-      final container3 = ContainerNode();
+      final container = DummyNode();
+      final container2 = DummyNode();
+      final container3 = DummyNode();
 
-      root.connect(NodeConnDetails(input, 0), {NodeConnDetails(container, 1)});
+      root.connect(container.outputProps.first, {container2.inputProps.first});
+      root.connect(container2.outputProps.first, {container.inputProps.first});
 
-      root.connect(
-          NodeConnDetails(container, 0), {NodeConnDetails(container3, 1)});
+      root.connect(input.outputProps.first, {container.inputProps[1]});
+      root.connect(container.outputProps.first, {container3.inputProps.first});
+      root.connect(container2.outputProps.first, {output.inputProps.first});
 
-      root.connect(
-          NodeConnDetails(container2, 0), {NodeConnDetails(output, 0)});
+      expect(container.inputProps[0].data, isNull);
+      expect(container.inputProps[1].data, isNotNull);
+      expect(container2.inputProps[0].data, isNull);
+    });
+
+    test('cut edge end cycle', () {
+      final root = RootNode();
+      final input = InputGroup();
+      final dummy = DummyNode();
+      final dummy2 = DummyNode();
+
+      root.connect(input.outputProps.first, {dummy.inputProps.first});
+      root.connect(input.outputProps.first, {dummy2.inputProps[1]});
+      root.connect(dummy.outputProps.first, {dummy2.inputProps.first});
+      root.connect(dummy2.outputProps.first, {dummy.inputProps[1]});
+
+      expect(dummy.inputProps[0].data, isNotNull);
+      expect(dummy.inputProps[1].data, isNull);
+      expect(dummy2.inputProps[0].data, isNull);
+
+      root.disconnect(dummy.outputProps.first, {dummy2.inputProps.first});
+      expect(dummy.inputProps[1].data, isNotNull);
     });
   });
 }
