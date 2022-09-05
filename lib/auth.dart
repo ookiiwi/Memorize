45942dfdfaed7948fb115c240225709277386eb3
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-09-05 22:18:40 +0200

    Refactor Auth methods to make use of new server model

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,69 +1,63 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:crypto/crypto.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 enum UserConnectionStatus { loggedIn, loggedOut, failed }
 
 class UserInfo {
-  UserInfo(this.username, String password, {String? email})
-      : password = sha256.convert(utf8.encode(password)).toString(),
-        email = email ?? '';
+  const UserInfo({this.email, this.username, required this.pwd})
+      : assert(email != null || username != null);
 
-  UserInfo._(this.username, this.password,
-      {String? email, UserConnectionStatus? status})
-      : email = email ?? '' {
-    if (status != null) this.status = status;
-  }
+  final String? email;
+  final String? username;
+  final String pwd;
+  get password => pwd;
 
-  factory UserInfo.fromJson(Map<String, dynamic> json) {
-    return UserInfo._(json['username'], json['pwd'],
-        email: json['email'],
-        status: UserConnectionStatus.values[json['status']]);
-  }
-
-  static Future<UserInfo?> retrieve() async {
-    final prefs = await SharedPreferences.getInstance();
-
-    String? data = prefs.getString(_prefKey);
-    return data != null ? UserInfo.fromJson(jsonDecode(data)) : null;
-  }
-
-  Map<String, dynamic> toJson() => {
-        'email': email,
-        'username': username,
-        'pwd': password,
-        'status': status.index
-      };
-
-  final String email;
-  final String username;
-  final String password;
-  UserConnectionStatus status = UserConnectionStatus.loggedOut;
-  static const String _prefKey = 'userInfo';
-
-  void save() async {
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setString(_prefKey, jsonEncode(toJson()));
-  }
-
-  void logout() async {
-    final prefs = await SharedPreferences.getInstance();
-    prefs.remove(_prefKey);
-    status = UserConnectionStatus.loggedOut;
-  }
+  Map<String, dynamic> toJson() =>
+      {'email': email, 'username': username, 'password': pwd};
 }
 
 class Auth extends ChangeNotifier {
+  static const storage = FlutterSecureStorage();
+
+  static Future<UserConnectionStatus> retrieveState() async {
+    UserConnectionStatus ret = UserConnectionStatus.loggedOut;
+
+    try {
+      final token = await storage.read(key: 'jwt');
+
+      var response = await http.get(
+          Uri.parse("http://localhost:3000/isLoggedIn"),
+          headers: {'Authorization': 'Bearer $token'});
+
+      print(response.statusCode);
+      print(response.reasonPhrase);
+      print(response.body);
+
+      if (response.statusCode == 200) ret = UserConnectionStatus.loggedIn;
+    } on SocketException {
+      print('No Internet connection 😑');
+    } on HttpException {
+      print("Couldn't find the post 😱");
+    } on FormatException {
+      print("Bad response format 👎");
+    } catch (e) {
+      print('auth error: $e');
+    }
+
+    return ret;
+  }
+
   static Future<UserConnectionStatus> register(UserInfo userInfo) async {
     UserConnectionStatus ret = UserConnectionStatus.failed;
 
     try {
       var response = await http.post(
-          Uri.parse("http://192.168.1.12/front_end_api/session.php"),
-          body: jsonEncode({'flag': 'r', 'data': userInfo.toJson()}));
+          Uri.parse("http://localhost:3000/auth/signup"),
+          headers: {'Content-Type': 'application/json; charset=UTF-8'},
+          body: jsonEncode(userInfo));
 
       print(response.statusCode);
       print(response.reasonPhrase);
@@ -88,15 +82,20 @@ class Auth extends ChangeNotifier {
 
     try {
       var response = await http.post(
-          Uri.parse("http://192.168.1.12/front_end_api/session.php?dbg=true"),
-          body: jsonEncode({'flag': 'l', 'data': userInfo.toJson()}));
+          Uri.parse("http://localhost:3000/auth/login"),
+          headers: {'Content-Type': 'application/json; charset=UTF-8'},
+          body: jsonEncode(userInfo));
 
       print(response.statusCode);
       print(response.reasonPhrase);
       print(response.body);
+      print(response.headers);
 
-      if (response.statusCode == 200 && response.body == 'success') {
+      if (response.statusCode == 200) {
         ret = UserConnectionStatus.loggedIn;
+        final token = jsonDecode(response.body)['token'];
+        print('token extracted: $token');
+        storage.write(key: 'jwt', value: token);
       }
     } on SocketException {
       print('No Internet connection 😑');
@@ -111,7 +110,7 @@ class Auth extends ChangeNotifier {
     return ret;
   }
 
-  static void logout(UserInfo userInfo) {
-    userInfo.logout();
+  static void logout() {
+    storage.delete(key: 'jwt');
   }
 }
