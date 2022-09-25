438df41259aae9a5668da66f4e89ff445cb22707
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-09-25 16:59:02 +0200

    Add addon routes

--- backend/routes/addon.js
+++ backend/routes/addon.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const upload = require('../middleware/multer-config');
+const addonCtrl = require('../controllers/addon');
 
 router.use((req, res, next) => {
     req.params.dest = 'addon';
     next();
 });
 
+router.get('/', addonCtrl.search); // list all addons available or matching names if search query param
+router.get('/:id', addonCtrl.get); // return an addon
 router.post('/', auth, upload);
-router.get('/:id', auth, listCtrl.get);
-router.put('/:id', auth, upload, listCtrl.update);
+router.put('/:id', auth, upload);
 
 module.exports = router;
