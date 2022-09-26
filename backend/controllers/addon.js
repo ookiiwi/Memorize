b0076086fdae65adb0bf1644a6f47e3e6e0e4e4d
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-09-26 20:25:31 +0200

    Implement get function

--- backend/controllers/addon.js
+++ backend/controllers/addon.js
@@ -1,8 +1,14 @@
 const fu = require('../utils/file-utils');
+const path = require('path');
+const fs = require('fs');
 
 exports.search = (req, res, next) => {
     req.params.dir = '/shared/addon';
     fu.search(req,res,next);
 };
 
-exports.get = (req,res,next) => {};
+exports.get = (req, res, next) => {
+    const p = path.join(__dirname, '../storage/shared/addon/' + req.params.id);
+    const file = fs.readFileSync(p).toString();
+    res.status(200).json(file);
+};
