425cf9c29ef1fb8e1992ef9540c9b47595910af0
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-09-28 15:43:10 +0200

    Introduce schema to specify data format to addons

--- lib/addon.dart
+++ lib/addon.dart
@@ -1,31 +1,23 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:memorize/menu.dart' as menu;
+import 'package:http_parser/http_parser.dart';
+import 'package:memorize/auth.dart';
 import 'package:memorize/web/node.dart';
 import 'package:memorize/web/node_base.dart';
 import 'package:memorize/widget.dart';
+import 'package:universal_io/io.dart';
+
+typedef AddonSchema = Map<String, Set<String>>;
 
 class AddonUtil {
   static Addon fromJson(Map<String, dynamic> json) {
     switch (json['type']) {
-      case 'LanguageAddon':
-        return LanguageAddon.fromJson(json);
+      case 'SchemaAddon':
+        return SchemaAddon.fromJson(json);
       default:
-        throw Exception();
-    }
-  }
-}
-
-class AddonOptionUtil {
-  static AddonOption fromJson(Map<String, dynamic> json) {
-    switch (json['type']) {
-      case 'AddonTextOption':
-        return AddonTextOption.fromJson(json);
-      case 'AddonCheckBoxListOption':
-        return AddonCheckBoxListOption.fromJson(json);
-      case 'AddonDropdownOption':
-        return AddonDropdownOption.fromJson(json);
-      default:
-        throw FlutterError('${json['type']} is not a valid addon option type');
+        throw FlutterError("Unknown addon type: '${json['type']}'");
     }
   }
 }
@@ -47,194 +39,240 @@ class AddonNode extends StatelessWidget {
   Widget build(BuildContext context) => child;
 }
 
-abstract class AddonOption {
-  AddonOption(
-      {required this.title, required this.value, this.flags = EDITABLE});
-  AddonOption.fromJson(Map<String, dynamic> json)
-      : title = json['title'],
-        flags = json['flags'];
-
-  Map<String, dynamic> toJson() =>
-      {'title': title, 'flags': flags, 'type': runtimeType.toString()};
-
-  final String title;
-  dynamic value;
-  int flags;
-
-  static const int EDIT_MODE = (1 << 0);
-  static const int EDITABLE = (1 << 1);
-
-  Widget build(BuildContext context, {bool editMode = false});
-}
-
-class AddonDropdownOption extends AddonOption {
-  AddonDropdownOption({
-    required super.title,
-    required List<String> items,
-  }) : super(value: items);
-
-  AddonDropdownOption.fromJson(
-    Map<String, dynamic> json,
-  ) : super.fromJson(json) {
-    value = json['value'];
-  }
-
-  @override
-  Map<String, dynamic> toJson() => super.toJson()..addAll({'value': value});
-
-  @override
-  Widget build(BuildContext context, {bool editMode = false}) {
-    return menu.DropDownMenuManager(
-        child: menu.DropDownMenu(items: [
-      List.from(value.map((e) => menu.MenuItem(
-          text: e,
-          onTap: () {
-            throw UnimplementedError();
-          })))
-    ]));
-  }
-}
-
-class AddonTextOption extends AddonOption {
-  AddonTextOption({
-    required super.title,
-    String value = '',
-    super.flags,
-  }) : super(value: value);
-
-  AddonTextOption.fromJson(Map<String, dynamic> json) : super.fromJson(json) {
-    value = json['value'];
-  }
-
-  @override
-  Map<String, dynamic> toJson() => super.toJson()..addAll({'value': value});
-
-  @override
-  Widget build(BuildContext context, {bool editMode = false}) {
-    return (flags & AddonOption.EDITABLE) > 0
-        ? TextField(
-            controller: TextEditingController(text: value),
-            onChanged: (newVal) {
-              value = newVal;
-            },
-          )
-        : Text(value);
-  }
-}
-
-class AddonCheckBoxListOption extends AddonOption {
-  AddonCheckBoxListOption(
-      {required super.title, super.flags, required Map<String, bool> value})
-      : super(value: value);
-
-  AddonCheckBoxListOption.fromJson(Map<String, dynamic> json)
-      : super.fromJson(json) {
-    value = Map<String, bool>.from(json['value']);
-  }
-
-  @override
-  Map<String, dynamic> toJson() => super.toJson()..addAll({'value': value});
-
-  final _notifier = ValueNotifier(false);
-
-  @override
-  Widget build(BuildContext context, {bool editMode = false}) {
-    return ValueListenableBuilder(
-        valueListenable: _notifier,
-        builder: (context, _, child) => ExpandedWidget(
-            isExpanded: true,
-            duration: const Duration(milliseconds: 100),
-            sectionTitle: title,
-            child: Column(
-              children: [
-                ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: value.length,
-                    itemBuilder: (context, i) => Container(
-                        padding: const EdgeInsets.all(5),
-                        child: Row(
-                          children: [
-                            if (!editMode)
-                              Checkbox(
-                                  value: value[value.keys.elementAt(i)],
-                                  onChanged: (newVal) {
-                                    if (newVal == null) return;
-                                    value[value.keys.elementAt(i)] = newVal;
-                                    _notifier.value = !_notifier.value;
-                                  }),
-                            // check if text edition flag is set
-                            (flags &
-                                        (AddonOption.EDIT_MODE |
-                                            AddonOption.EDITABLE)) >
-                                    0
-                                ? Expanded(
-                                    child: TextField(
-                                    controller: TextEditingController(
-                                        text: value.keys.elementAt(i)),
-                                    onSubmitted: (newVal) {
-                                      value[newVal] = value
-                                          .remove(value.keys.elementAt(i))!;
-                                    },
-                                  ))
-                                : Text(value.keys.elementAt(i))
-                          ],
-                        ))),
-                FloatingActionButton(
-                  onPressed: () {
-                    value.addAll({'': true});
-                    _notifier.value = !_notifier.value;
-                  },
-                  child: const Icon(Icons.add),
-                )
-              ],
-            )));
-  }
-}
-
 abstract class AddonBuildOptions {}
 
 abstract class Addon {
-  Addon(this.name, {this.node, required List<AddonOption> options})
-      : _options = options;
+  Addon(this.name, {this.node});
   Addon.fromJson(Map<String, dynamic> json)
       : name = json['name'],
         node = json['node'] != null
             ? AddonNode.fromJson(json: json['node'])
-            : null,
-        _options =
-            List.from(json['options'].map((e) => AddonOptionUtil.fromJson(e)));
+            : null;
 
   Map<String, dynamic> toJson() {
     return {
       'type': runtimeType.toString(),
       'name': name,
-      'options': _options.map((e) => e.toJson()).toList(),
-      'node': node?.toJson()
+      'node': node?.toJson(),
     };
   }
 
   String name;
   AddonNode? node;
 
-  final List<AddonOption> _options;
-  List<AddonOption> get options => _options;
-
   Widget build([AddonBuildOptions? options]);
+  Widget buildOptions({bool edit = false});
+
+  static Future<Addon?> fetch(String name) async {
+    try {
+      final response = await dio.get('http://localhost:3000/addon/' + name);
+      final json = jsonDecode(response.data);
+
+      return AddonUtil.fromJson(json);
+    } on SocketException {
+      print('No Internet connection 😑');
+    } on HttpException {
+      print("Couldn't find the post 😱");
+    } on FormatException {
+      print("Bad response format 👎");
+    } catch (e) {
+      print('An error occured during addon fetch: $e');
+    }
+
+    return null;
+  }
+
+  void upload() async {
+    try {
+      final formData = FormData.fromMap({
+        'file': MultipartFile.fromString(jsonEncode(this),
+            filename: name, contentType: MediaType("application", "json"))
+      });
+
+      await dio.post('http://localhost:3000/addon', data: formData);
+    } on SocketException {
+      print('No Internet connection 😑');
+    } on HttpException {
+      print("Couldn't find the post 😱");
+    } on FormatException {
+      print("Bad response format 👎");
+    } catch (e) {
+      print('An error occured during addon upload: $e');
+    }
+  }
+
+  void register();
+  void unregister();
 }
 
-class LanguageAddon extends Addon {
-  LanguageAddon(super.name, {super.node})
-      : super(options: [
-          AddonTextOption(title: 'name', value: name),
-          AddonCheckBoxListOption(
-              title: 'languages', value: {'fr': true, 'en': true})
-        ]);
+class SchemaAddon extends Addon {
+  SchemaAddon(super.name, {super.node}) : schemas = {} {
+    _checkInit();
+  }
 
-  LanguageAddon.fromJson(Map<String, dynamic> json) : super.fromJson(json);
+  SchemaAddon.fromJson(Map<String, dynamic> json)
+      : schemas = Set.from(json['schemas']),
+        super.fromJson(json) {
+    _checkInit();
+  }
+
+  @override
+  Map<String, dynamic> toJson() => super.toJson()
+    ..addAll({
+      'schemas': schemas.toList(),
+    });
+
+  static Set<String> availableSchemas = {};
+  static bool _availableSchemasInit = false;
+
+  final Set<String> schemas;
+
+  static String storageDir = 'addon/schema/';
+
+  final schemaNotifier = ValueNotifier(false);
+  final _schemaTextFieldController = TextEditingController();
+
+  static void _checkInit() {
+    assert(_availableSchemasInit);
+  }
+
+  static void init() {
+    _initSchemas();
+  }
+
+  static void _initSchemas() async {
+    if (_availableSchemasInit) return;
+
+    final varKey = storageDir + 'availableSchemas';
+
+    availableSchemas = Set.from(
+        jsonDecode((await Auth.storage.read(key: varKey)) ?? '["fr", "en"]'));
+
+    _availableSchemasInit = true;
+
+    availableSchemas = {"fr", "en"};
+
+    try {
+      const url = 'http://localhost:3000/addon/schema';
+      // TODO: retrieve schemas from server
+    } on SocketException {
+      print('No Internet connection 😑');
+    } on HttpException {
+      print("Couldn't find the post 😱");
+    } on FormatException {
+      print("Bad response format 👎");
+    } catch (e) {
+      print('Error when trying to retrieve available schemas : $e');
+    }
+
+    Auth.storage
+        .write(key: varKey, value: jsonEncode(availableSchemas.toList()));
+  }
 
   @override
   Widget build([AddonBuildOptions? options]) {
     assert(node != null);
     return node!;
+  }
+
+  @override
+  Widget buildOptions({bool edit = false}) {
+    return ExpandedWidget(
+      sectionTitle: 'Schemas',
+      isExpanded: true,
+      duration: const Duration(milliseconds: 100),
+      child:
+
+          // name field
+          // schema field
+          Column(children: [
+        Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: TextField(
+              controller: _schemaTextFieldController,
+              onChanged: (value) {
+                // search schema
+              },
+              onSubmitted: (value) {
+                schemas.add(value);
+                schemaNotifier.value = !schemaNotifier.value;
+                _schemaTextFieldController.clear();
+              },
+            )),
+        ValueListenableBuilder(
+            valueListenable: schemaNotifier,
+            builder: (context, _, __) => ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: schemas.length,
+                  itemBuilder: (context, i) => Container(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      alignment: Alignment.center,
+                      child: Text(schemas.elementAt(i))),
+                ))
+      ]),
+    );
+  }
+
+  @override
+  void register() async {
+    final AddonSchema localSchema = Map.from(jsonDecode(
+        (await Auth.storage.read(key: storageDir + 'config')) ?? '{}'));
+
+    for (var schema in schemas) {
+      if (!localSchema.containsKey(schema)) {
+        localSchema[schema] = {};
+      }
+
+      localSchema[schema]!.add(name);
+
+      Auth.storage.write(key: storageDir + name, value: jsonEncode(toJson()));
+    }
+
+    _updateGlobalSchema(localSchema);
+  }
+
+  @override
+  void unregister() async {
+    final temp = await Auth.storage.read(key: storageDir + 'config');
+
+    if (temp == null) return;
+
+    final AddonSchema localSchema = jsonDecode(temp);
+
+    for (var schema in schemas) {
+      localSchema[schema]?.remove(name);
+    }
+
+    // TODO: update global
+  }
+
+  /// Add local schema to global one
+  static void _updateGlobalSchema(AddonSchema localSchema) async {
+    try {
+      const url = 'http://localhost:3000/addon/schema';
+      final schemaResponse = await dio.get(url);
+
+      final AddonSchema globalSchema = Map.from(schemaResponse.data['schema'])
+          .map((key, value) => MapEntry(key, Set.from(value)));
+
+      for (var e in localSchema.entries) {
+        globalSchema.putIfAbsent(e.key, () => {}).addAll(e.value);
+      }
+
+      print('global: $globalSchema');
+
+      await dio.put(url, data: {
+        'schema':
+            globalSchema.map((key, value) => MapEntry(key, value.toList()))
+      });
+    } on SocketException {
+      print('No Internet connection 😑');
+    } on HttpException {
+      print("Couldn't find the post 😱");
+    } on FormatException {
+      print("Bad response format 👎");
+    } catch (e) {
+      print('An error occured during addon upload: $e');
+    }
   }
 }
