ce2eaffdb44a8d030e5a268c27598efda7078c0d
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-09-28 16:49:57 +0200

    Add schema support and addon update

--- backend/controllers/addon.js
+++ backend/controllers/addon.js
@@ -2,6 +2,8 @@ const fu = require('../utils/file-utils');
 const path = require('path');
 const fs = require('fs');
 
+const User = require('../models/user');
+
 exports.search = (req, res, next) => {
     req.params.dir = '/shared/addon';
     fu.search(req,res,next);
@@ -12,3 +14,39 @@ exports.get = (req, res, next) => {
     const file = fs.readFileSync(p).toString();
     res.status(200).json(file);
 };
+
+exports.getSchema = (req, res, next) => {
+    User.findById(req.auth.userId).then(
+        (user) => {
+            const schema = user.addon_schema;
+            res.status(201).json({ schema });
+        }
+    ).catch((err) => { 
+        console.log(err);
+        res.status(404).json({ err });
+    });
+};
+
+exports.updateSchema = (req, res, next) => {
+    User.findById(req.auth.userId).then(
+        (user) => {
+            user.addon_schema = req.body.schema;
+            user.save();
+        } 
+    ).catch((err) => { 
+        console.log(err);
+        res.status(404).json({ err });
+    });
+};
+
+exports.updateAddon = (req, res, next) => {
+    const newName = req.query.name;
+
+    if (newName){
+        const oldPath = path.join(__dirname, '../storage/shared/addon/' + req.params.id);
+        const newPath = path.join(__dirname, '../storage/shared/addon/' + newName);
+        fs.renameSync(oldPath, newPath);
+    }
+
+    res.status(201);
+}
--- backend/routes/addon.js
+++ backend/routes/addon.js
@@ -6,12 +6,17 @@ const addonCtrl = require('../controllers/addon');
 
 const destSet = (req, res, next) => {
     req.params.dest = 'addon';
+
     next();
 };
 
+router.get('/schema', auth, addonCtrl.getSchema);
+router.put('/schema', auth, addonCtrl.updateSchema);
+
 router.get('/', addonCtrl.search); // list all addons available or matching names if search query param
 router.get('/:id', addonCtrl.get); // return an addon
-router.post('/', auth, destSet, upload, (req, res) => { res.status(201) });
-router.put('/:id', auth, destSet, upload, (req, res) => { res.status(201) });
+
+router.post('/:id', auth, destSet, upload, (req, res) => { res.status(201) });
+router.put('/:id', auth, destSet, upload, addonCtrl.updateAddon);
 
 module.exports = router;
