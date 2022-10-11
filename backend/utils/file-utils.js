aec2b8798d46eb2f2b3822bb2420c57347fbcb90
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-10-11 17:33:24 +0200

    Map file to path when on search

--- backend/utils/file-utils.js
+++ backend/utils/file-utils.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const Group = require('../models/group');
 const File = require('../models/file');
+const shell = require('shelljs');
 
 exports.search = async (value, path, userId) => {
     if (value == null) {
@@ -36,8 +37,7 @@ exports.search = async (value, path, userId) => {
                     ]
                 }
             ]
-        }
-        , { name: 1 }, (err, file) => {
+        }, { name: 1 }, (err, file) => {
             if (err) {
                 console.log('err');
             } else {
@@ -45,45 +45,74 @@ exports.search = async (value, path, userId) => {
             }
         }).clone();
 
-    return files;
+    return files.map((e) => {
+        e.path = mapFileToPath(e._id, userId);
+        return { _id: e._id, name: e.name, path: e.path };
+    });
 };
 
-exports.testPermissions = async (file, userId) => {
+function mapFileToPath(fileId, userId) {
+    function find(path) {
+        const p = resolveUserstorage(path, userId);
+        const ret = shell.exec('find ' + p + ' -name ' + fileId);
 
+        if (ret.code !== 0) {
+            throw 'Shell error';
+        }
+
+        return ret.trim().split('\n').map((e) => unresolveUserStorage(e, userId));
+    }
+
+    return (find('/globalstorage')[0] || find('/userstorage')[0]);
+}
+
+exports.testPermissions = async (file, userId) => {
     let perm = file.permissions;
     let ret = perm & 3;
 
     if (ret == 3 || !userId) return ret;
     perm >>= 2;
-
+    
     const group = await Group.findById(file.group);
     if (group && group.users.has(userId)) {
         ret |= perm & 3;
     }
-
+    
     if (ret == 3) return ret;
-
-    // owner
+    perm >>= 2;
+    
     if (file.owner == userId) {
-        perm >>= 2;
         ret |= perm & 3;
     }
 
     return ret;
 }
 
-exports.resolveUserstorage = (path, id) => {
-    const ust = 'userstorage';
-    const gst = 'globalstorage';
-    let ret = path;
+exports.resolveUserstorage = resolveUserstorage;
 
-    if (path.startsWith('/' + ust)) {
+function resolveUserstorage(aPath, id) {
+    const ust = '/userstorage';
+    const gst = '/globalstorage';
+    let ret = aPath;
+
+    console.log('p:', aPath);
+
+    if (aPath.startsWith(ust)) {
         if (!id) throw "Forbidden access";
         ret = ret.replace(ust, 'user/' + id);
     }
-    else if (path.startsWith('/' + gst)) {
+    else if (aPath.startsWith(gst)) {
         ret = ret.replace(gst, 'global');
     }
+
+    return path.join(__dirname, '../storage/' + ret);
+}
+
+function unresolveUserStorage(path, id) {
+    let ret = path.replace(/.*\/global/, '/globalstorage');
+    if (id) ret = ret.replace(new RegExp('.*/user/' + id), '/userstorage');
+
+    console.log('unresolve', path, 'to', ret);
 
     return ret;
 }
