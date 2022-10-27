22cdcce01d71369eb31eb790919611142829d891
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-10-27 14:34:07 +0200

    Add dict route

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 const fileRoutes = require('./routes/file');
 const dirRoutes = require('./routes/directory');
+const dictRoutes = require('./routes/dict');
 const auth = require('./middleware/auth');
 const app = express();
+const cors = require('cors');
 const port = 3000;
 
 mongoose.connect('mongodb+srv://admin:bX3GHQlOtCHaSPOP@memodb.fsc75sv.mongodb.net/?retryWrites=true&w=majority', 
@@ -14,13 +16,12 @@ mongoose.connect('mongodb+srv://admin:bX3GHQlOtCHaSPOP@memodb.fsc75sv.mongodb.ne
     .then(() => console.log('MongoDB connection succeeded !'))
     .catch(() => console.log('MongoDB connection failed !'));
 
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
+mongoose.connection.on('disconnected', err => {
+  console.error(err);
+  console.log('Mongoose error');
 });
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,7 +30,13 @@ app.get('/isLoggedIn', auth, (req, res) => { res.send('check if logged'); });
 app.use('/auth', userRoutes);
 app.use('/file', fileRoutes);
 app.use('/dir', dirRoutes);
+app.use('/dict', dictRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+});
+
+app.on('uncaughtException', (err) => {
+  console.error(err);
+  console.log("Node not exiting");
 });
