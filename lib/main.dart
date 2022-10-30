68dbf170b0097f3d6d28c503bda52e919c4e192c
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-10-30 17:59:29 +0100

    Move DataLoader here

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:memorize/auth.dart';
 import 'package:memorize/data.dart';
+import 'package:memorize/file_system.dart' as fs;
+import 'package:memorize/list_explorer.dart';
 
 import 'package:memorize/mobile/tab.dart'
     if (dart.library.js) 'package:memorize/web/tab.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 import 'package:memorize/ad_state.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -71,5 +78,47 @@ class _SplashScreen extends State<SplashScreen> {
             return widget.builder(context);
           }
         });
+  }
+}
+
+class DataLoader {
+  static bool _isDataLoaded = false;
+
+  static load({bool force = false}) async {
+    if (_isDataLoaded && !force) return;
+    // TODO: check if user logged here
+
+    sharedPrefInstance = await SharedPreferences.getInstance();
+    final isFirstRun = sharedPrefInstance.getBool('isFirstRun');
+    print('isFirstRun? $isFirstRun');
+
+    await Auth.init();
+    await fs.init(isFirstRun == null || isFirstRun);
+
+    if (isFirstRun == null || isFirstRun) {
+      ListExplorer.init();
+
+      final manifestContent = await rootBundle.loadString('AssetManifest.json');
+      final Map<String, dynamic> manifestMap = jsonDecode(manifestContent);
+
+      final imagePaths = manifestMap.keys
+          .where((key) => key.contains('assets/profil_icons/'))
+          .toList();
+
+      sharedPrefInstance.setBool('isFirstRun', false);
+      await sharedPrefInstance.setString(
+          'profil_icons', jsonEncode(imagePaths));
+    }
+
+    final usrData = await secureStorage.read(key: 'userData');
+
+    if (usrData != null) {
+      userData = UserData.fromJson(jsonDecode(usrData));
+    } else {
+      userData = UserData();
+      secureStorage.write(key: 'userData', value: userData.toString());
+    }
+
+    _isDataLoaded = true;
   }
 }
