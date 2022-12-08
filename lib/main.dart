ff4ca22cf6e6f7f601e5cfa5a227940fdba70386
Zakariya Hadrami <zakariyah.dev@gmail.com>
2022-12-08 11:18:03 +0100

    Add custom theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:memorize/bloc/auth_bloc.dart';
 import 'package:memorize/bloc/connection_bloc.dart' as cb;
 import 'package:memorize/loggers/offline_logger.dart';
@@ -14,7 +16,7 @@ import 'package:memorize/views/mobile/main.dart'
     if (dart.library.js) 'package:memorize/views/web/main.dart';
 import 'package:memorize/services/auth_service.dart';
 import 'package:memorize/storage.dart';
-import 'package:memorize/widget.dart';
+import 'package:memorize/widgets/bar.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
@@ -51,8 +53,36 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  final _brightness = ValueNotifier(Brightness.dark);
-  set brightness(Brightness brightness) => _brightness.value = brightness;
+  final _themeMode = ValueNotifier(ThemeMode.light);
+  set themeMode(ThemeMode mode) => _themeMode.value = mode;
+  ThemeMode get themeMode => _themeMode.value;
+
+  final flexScheme = const FlexSchemeData(
+    name: 'Panda',
+    description: 'Panda color theme',
+    light: FlexSchemeColor(
+      primary: Colors.black,
+      secondary: Color(0xFFa0c284),
+    ),
+    dark: FlexSchemeColor(
+      primary: Colors.white,
+      secondary: Color(0xFFa0c284),
+    ),
+  );
+
+  final String fontFamily = 'ZenMaruGothic';
+
+  ThemeData get flexLightTheme => FlexThemeData.light(
+        fontFamily: fontFamily,
+        colors: flexScheme.light,
+        useMaterial3: true,
+      );
+
+  ThemeData get flexDarkTheme => FlexThemeData.dark(
+        fontFamily: fontFamily,
+        colors: flexScheme.dark,
+        useMaterial3: true,
+      );
 
   static MyApp of(BuildContext context) {
     final ret = context.findAncestorWidgetOfExactType<MyApp>();
@@ -91,17 +121,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder<Brightness>(
-      valueListenable: _brightness,
+    //const usedScheme = FlexScheme.sakura;
+    //const usedScheme = FlexScheme.outerSpace;
+    //const usedScheme = FlexScheme.blumineBlue;
+    //const usedScheme = FlexScheme.hippieBlue;
+    //const usedScheme = FlexScheme.mallardGreen;
+    //const usedScheme = FlexScheme.mango;
+    //const usedScheme = FlexScheme.sanJuanBlue;
+    //const usedScheme = FlexScheme.vesuviusBurn;
+
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: _themeMode,
       builder: (context, value, child) => MaterialApp.router(
         debugShowCheckedModeBanner: false,
         title: 'Memo',
-        theme: ThemeData(
-          colorSchemeSeed: const Color(0xff006498),
-          brightness: value,
-          useMaterial3: true,
-          fontFamily: 'FiraSans',
-        ),
+        themeMode: value,
+        theme: flexLightTheme,
+        darkTheme: flexDarkTheme,
         routerConfig: router,
       ),
     );
