05f1f6fb8b4a763e947b0545716cc768a2c4b772
Zakariya Hadrami <zakariyah.dev@gmail.com>
2023-01-19 17:57:13 +0100

    Add dictionary section

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:memorize/services/dict/dict.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -10,7 +11,49 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPage extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return const Center(child: Text('Settings page not implemented yet'));
+    return ListView(
+      children: [
+        ListTile(
+          title: const Text('Dictionaries'),
+          trailing: const Icon(
+            Icons.arrow_forward_ios,
+            size: 16,
+          ),
+          onTap: () => Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const DictionaryPage(),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class DictionaryPage extends StatefulWidget {
+  const DictionaryPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _DictionaryPage();
+}
+
+class _DictionaryPage extends State<DictionaryPage> {
+  @override
+  Widget build(BuildContext context) {
+    final installedTargets = Dict.listTargets();
+    return ListView(
+      padding: const EdgeInsets.only(bottom: kBottomNavigationBarHeight),
+      children: installedTargets
+          .map(
+            (e) => ListTile(
+              title: Text(e),
+              trailing: IconButton(
+                onPressed: () => setState(() => Dict.remove(e)),
+                icon: const Icon(Icons.cancel_outlined),
+              ),
+            ),
+          )
+          .toList(),
+    );
   }
 }
