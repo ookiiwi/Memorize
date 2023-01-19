b9ac06d1838c902cf8c50af70f74acd963df4276
Zakariya Hadrami <zakariyah.dev@gmail.com>
2023-01-19 18:00:52 +0100

    Implement target listing, download and remove

--- lib/services/dict/dict.dart
+++ lib/services/dict/dict.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:isolate';
 
-import 'package:flutter/services.dart';
+import 'package:dio/dio.dart';
 import 'package:memorize/app_constants.dart';
 import 'package:dico/dico.dart';
 
@@ -29,50 +28,6 @@ class Dict {
     return ret;
   }
 
-  static Stream<String> getAll(Iterable<DicoId> ids, String target) async* {
-    final dir = applicationDocumentDirectory;
-    final reader = Reader('$dir/dict/$target.$_fileExtension');
-
-    for (var id in ids) {
-      yield await Future.microtask(() => _get([id, reader]));
-    }
-
-    print('getAll close reader');
-    reader.close();
-  }
-
-  static Stream<String> getAllIso(Iterable<DicoId> ids, String target) {
-    final dir = applicationDocumentDirectory;
-    final reader = Reader('$dir/dict/$target.$_fileExtension');
-    final p = ReceivePort('Dict.getAll');
-
-    final stopwatch = Stopwatch()..start();
-
-    Isolate.spawn(_getAll, [p.sendPort, ids, reader, true])
-        .whenComplete(() => print('isolate spawn took ${stopwatch.elapsed}'));
-
-    return Stream.castFrom(p.asBroadcastStream());
-  }
-
-  static void _getAll(List args) {
-    SendPort responsePort = args[0];
-    Iterable<DicoId> ids = args[1];
-    Reader reader = args[2];
-    bool closeReader = args[3];
-
-    for (var id in ids) {
-      final res = _get([id, reader]);
-      responsePort.send(res);
-    }
-
-    if (closeReader) {
-      print('_getAll close reader');
-      reader.close();
-    }
-
-    Isolate.exit();
-  }
-
   static String _get(List args) {
     final id = args[0];
     final reader = args[1];
@@ -81,15 +36,67 @@ class Dict {
     return utf8.decode(ret);
   }
 
-  static Future<void> check(String target) async {
-    final file =
-        File('$applicationDocumentDirectory/dict/$target.$_fileExtension');
+  static bool exists(String target) {
+    final filename =
+        '$applicationDocumentDirectory/dict/$target.$_fileExtension';
+    final file = File(filename);
 
-    if (file.existsSync()) return;
+    return file.existsSync();
+  }
 
-    // download target*.dico
-    final buf = await rootBundle.load('assets/dict/$target.$_fileExtension');
-    file.createSync(recursive: true);
-    file.writeAsBytesSync(buf.buffer.asUint8List());
+  static Future<void> download(String target) async {
+    final filename =
+        '$applicationDocumentDirectory/dict/$target.$_fileExtension';
+
+    try {
+      final dio = Dio();
+
+      await dio.download(
+        'http://192.168.1.13:8080/dictionaries/$target.$_fileExtension',
+        filename,
+      );
+    } on DioError {
+      rethrow;
+    }
+  }
+
+  static void remove(String target) {
+    final filename =
+        '$applicationDocumentDirectory/dict/$target.$_fileExtension';
+    final file = File(filename);
+
+    assert(file.existsSync());
+
+    file.deleteSync();
+  }
+
+  static Iterable<String> listTargets() {
+    final dir = Directory('$applicationDocumentDirectory/dict/');
+
+    if (!dir.existsSync()) return [];
+
+    return dir.listSync().fold([], (p, e) {
+      final name = e.path.split('/').last;
+
+      return [...p, if (!name.startsWith('.')) name.replaceFirst('.dico', '')];
+    });
+  }
+
+  static Future<List<String>> listRemoteTargets() async {
+    final dio = Dio();
+
+    try {
+      final response = await dio.get('http://192.168.1.13:8080/dictionaries/');
+      final String body = response.data;
+
+      final exp =
+          RegExp(r'<td class="display-name"><a href=".*?">(.*)<\/a><\/td>');
+      final matches = exp.allMatches(body);
+
+      return matches.map((e) => e.group(1)!.replaceFirst('.dico', '')).toList()
+        ..removeWhere((e) => e.endsWith('/'));
+    } on DioError {
+      return listTargets().toList();
+    }
   }
 }
