5b537534c48b23ff3b79858669cc9cc82a4606fe
Zakariya Hadrami <zakariyah.dev@gmail.com>
2023-03-16 01:20:46 +0100

    Add iso-639 mapping helper

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -1,2 +1,20 @@
 late final String applicationDocumentDirectory;
 late final String temporaryDirectory;
+
+class IsoLanguage {
+  static const langMapping = {
+    'jpn': {'name': 'Japanese', 'nativeName': '日本語'},
+    'eng': {'name': 'English', 'nativeName': 'English'},
+    'fra': {'name': 'French', 'nativeName': 'Français'},
+    'afr': {'name': 'Afrikaans', 'nativeName': 'Afrikaans'},
+    'deu': {'name': 'German', 'nativeName': 'Deutsh'}
+  };
+
+  static String getFullname(String code) {
+    if (!langMapping.containsKey(code)) {
+      throw Exception("Iso code '$code' not supported");
+    }
+
+    return langMapping[code]!['name']!;
+  }
+}
