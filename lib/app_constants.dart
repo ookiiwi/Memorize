466e593753877e236559e3756de329abadb9246e
Zakariya Hadrami <zakariyah.dev@gmail.com>
2023-03-21 18:30:56 +0100

    Add initConstants

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -1,5 +1,22 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pocketbase/pocketbase.dart';
+
 late final String applicationDocumentDirectory;
 late final String temporaryDirectory;
+late final String host;
+late final PocketBase pb;
+
+Future<void> initConstants() async {
+  await dotenv.load();
+
+  applicationDocumentDirectory =
+      (await getApplicationDocumentsDirectory()).path;
+  temporaryDirectory = (await getTemporaryDirectory()).path;
+
+  host = dotenv.env['HOST']!;
+  pb = PocketBase('http://$host:8090');
+}
 
 class IsoLanguage {
   static const langMapping = {
