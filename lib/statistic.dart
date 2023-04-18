f61d00eff6c19ffc9633de8bf0cc6a93aaa1e1d8
Zakariya Hadrami <zakariyah.dev@gmail.com>
2023-04-18 16:25:04 +0200

    Adjust counts if time diff

--- lib/statistic.dart
+++ lib/statistic.dart
@@ -3,25 +3,41 @@ import 'dart:io';
 
 import 'package:memorize/app_constants.dart';
 
+extension DateTimeExtension on DateTime {
+  int get weekOfMonth {
+    var date = this;
+    final firstDayOfTheMonth = DateTime(date.year, date.month, 1);
+    int sum = firstDayOfTheMonth.weekday - 1 + date.day;
+    if (sum % 7 == 0) {
+      return sum ~/ 7;
+    } else {
+      return sum ~/ 7 + 1;
+    }
+  }
+}
+
 class GlobalStats {
   static final filepath = '$applicationDocumentDirectory/stats/global';
 
   GlobalStats({
-    this.newEntriesWeek = 0,
-    this.newEntriesMonth = 0,
-    this.newEntriesYear = 0,
-    this.newEntriesAllTime = 0,
     this.overallScore = 0,
     this.scoreCount = 0,
-  });
+  })  : _newEntriesWeek = 0,
+        _newEntriesMonth = 0,
+        _newEntriesYear = 0,
+        _newEntriesAllTime = 0,
+        _time = DateTime.now();
 
   GlobalStats.fromJson(Map<String, dynamic> json)
-      : newEntriesWeek = json['neWeek'],
-        newEntriesMonth = json['neMonth'],
-        newEntriesYear = json['neYear'],
-        newEntriesAllTime = json['neAllTime'],
+      : _newEntriesWeek = json['neWeek'],
+        _newEntriesMonth = json['neMonth'],
+        _newEntriesYear = json['neYear'],
+        _newEntriesAllTime = json['neAllTime'],
         overallScore = json['overallScore'],
-        scoreCount = json['scoreCount'];
+        scoreCount = json['scoreCount'],
+        _time = DateTime.fromMillisecondsSinceEpoch(json['time']) {
+    adjustCounts();
+  }
 
   factory GlobalStats.load() {
     final file = File(filepath);
@@ -31,12 +47,27 @@ class GlobalStats {
     //return GlobalStats();
   }
 
-  int newEntriesWeek;
-  int newEntriesMonth;
-  int newEntriesYear;
-  int newEntriesAllTime;
+  int _newEntriesWeek;
+  int _newEntriesMonth;
+  int _newEntriesYear;
+  int _newEntriesAllTime;
   double overallScore;
   int scoreCount;
+  DateTime _time;
+
+  void incrementEntries(int value) {
+    adjustCounts();
+
+    _newEntriesWeek += value;
+    _newEntriesMonth += value;
+    _newEntriesYear += value;
+    _newEntriesAllTime += value;
+  }
+
+  int get newEntriesWeek => _newEntriesWeek;
+  int get newEntriesMonth => _newEntriesMonth;
+  int get newEntriesYear => _newEntriesYear;
+  int get newEntriesAllTime => _newEntriesAllTime;
 
   double get normalizedScore =>
       scoreCount == 0 ? 0 : overallScore / (scoreCount * 100);
@@ -59,5 +90,22 @@ class GlobalStats {
     if (!file.existsSync()) file.createSync(recursive: true);
 
     file.writeAsStringSync(jsonEncode(this));
+  }
+
+  void adjustCounts() {
+    final time = DateTime.now();
+
+    if (time.year != _time.year) {
+      _newEntriesWeek = 0;
+      _newEntriesMonth = 0;
+      _newEntriesYear = 0;
+    } else if (time.month != _time.month) {
+      _newEntriesWeek = 0;
+      _newEntriesMonth = 0;
+    } else if (time.weekOfMonth != _time.weekOfMonth) {
+      _newEntriesWeek = 0;
+    }
+
+    _time = time;
   }
 }
