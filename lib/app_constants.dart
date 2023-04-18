e5ed33b01b8cd8b16e4d141ab94b5fd85eb86c54
Zakariya Hadrami <zakariyah.dev@gmail.com>
2023-04-18 15:00:40 +0200

    Add notifications handling and global stat

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -1,13 +1,25 @@
+import 'dart:convert';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_timezone/flutter_timezone.dart';
+import 'package:go_router/go_router.dart';
 import 'package:memorize/auth/auth.dart';
+import 'package:memorize/list.dart';
+import 'package:memorize/main.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pocketbase/pocketbase.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:memorize/statistic.dart';
 
 late final String applicationDocumentDirectory;
 late final String temporaryDirectory;
 late final String host;
 late final PocketBase pb;
+late final GlobalStats globalStats;
 final auth = Auth();
+final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 Future<void> initConstants() async {
   await dotenv.load();
@@ -18,6 +30,41 @@ Future<void> initConstants() async {
 
   host = dotenv.env['HOST']!;
   pb = PocketBase('http://$host:8090');
+
+  final timezone = await FlutterTimezone.getLocalTimezone();
+  tz.initializeTimeZones();
+  tz.setLocalLocation(tz.getLocation(timezone));
+
+  const androidInitSettings = AndroidInitializationSettings('app_icon');
+  const darwinInitSettings = DarwinInitializationSettings();
+  const initSettings = InitializationSettings(
+    android: androidInitSettings,
+    iOS: darwinInitSettings,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initSettings,
+      onDidReceiveNotificationResponse: (response) {
+    print('notif response ${response.id}');
+    print('notif response ${response.payload}');
+    print('notif response ${response.input}');
+
+    final payload = List.from(jsonDecode(response.payload!));
+    final filename = payload[0];
+
+    routerNavKey.currentContext
+        ?.push('/quiz_launcher', extra: MemoList.open(filename));
+  });
+
+  flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.requestPermission();
+
+  try {
+    globalStats = GlobalStats.load();
+  } catch (e) {
+    globalStats = GlobalStats();
+  }
 }
 
 class IsoLanguage {
