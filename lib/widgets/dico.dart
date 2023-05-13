e02c6e0ec64356bebd245ca5e4e10fd4e4c3209d
Zakariya Hadrami <zakariyah.dev@gmail.com>
2023-05-13 02:18:44 +0200

    Remove unused code

--- lib/widgets/dico.dart
+++ lib/widgets/dico.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:memorize/helpers/dict.dart';
-import 'package:memorize/list.dart';
 import 'package:memorize/widgets/entry/parser.dart';
 
 class DicoGetBuilder extends StatefulWidget {
@@ -36,81 +34,6 @@ class _DicoGetBuilder extends State<DicoGetBuilder> {
         } else if (snapshot.hasError) {
           return Center(
             child: Text('Error in DicoGetBuilder: ${snapshot.error}'),
-          );
-        }
-
-        return const Center(child: CircularProgressIndicator());
-      },
-    );
-  }
-}
-
-class DicoGetListViewBuilder extends StatefulWidget {
-  const DicoGetListViewBuilder({
-    super.key,
-    this.entries = const [],
-    required this.builder,
-  });
-
-  final List<ListEntry> entries;
-  final Widget Function(BuildContext, ListEntry) builder;
-
-  @override
-  State<StatefulWidget> createState() => _DicoGetListViewBuilder();
-}
-
-class _DicoGetListViewBuilder extends State<DicoGetListViewBuilder> {
-  Future<List<ListEntry>> fResults = Future.value([]);
-  bool isFutureSet = false;
-  List<ListEntry> results = [];
-
-  @override
-  void initState() {
-    super.initState();
-
-    List<Future<ListEntry>> fRes = [];
-
-    for (var e in widget.entries) {
-      final entry = DicoManager.get(e.target, e.id);
-
-      if (entry is Future<ParsedEntry>) {
-        fRes.add(entry.then((value) => e.copyWith(data: value)));
-      } else {
-        results.add(e.copyWith(data: entry));
-      }
-    }
-
-    if (fRes.isNotEmpty) {
-      fResults = Future.wait(fRes);
-      isFutureSet = true;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<List<ListEntry>>(
-      initialData: isFutureSet ? null : [],
-      future: isFutureSet ? fResults : null,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          assert(snapshot.data != null);
-          isFutureSet = false;
-
-          final List<ListEntry> entries =
-              results + List<ListEntry>.from(snapshot.data!);
-
-          return ListView.builder(
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            itemCount: entries.length,
-            itemBuilder: (context, i) => widget.builder(
-              context,
-              entries[i],
-            ),
-          );
-        } else if (snapshot.hasError) {
-          return Center(
-            child: Text('Error in DicoGetListViewBuilder: ${snapshot.error}'),
           );
         }
 
