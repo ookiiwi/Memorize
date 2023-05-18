8433baf8b2b98ef419dc29ecbf599a60530729da
Zakariya Hadrami <zakariyah.dev@gmail.com>
2023-05-19 00:27:28 +0200

    Add Future extension 'onResolve' and get target from parsed entry

--- lib/util.dart
+++ lib/util.dart
@@ -1,5 +1,22 @@
+import 'dart:async';
+
 import 'package:memorize/app_constants.dart';
 import 'package:memorize/memo_list.dart';
+import 'package:memorize/widgets/entry/parser.dart';
 
 String getTarget(MemoListItem item) =>
     'jpn-${appSettings.language}${item.isKanji ? '-kanji' : ''}';
+
+String getTargetFromParsedEntry(ParsedEntry entry) =>
+    'jpn-${appSettings.language}${entry is ParsedEntryJpnKanji ? '-kanji' : ''}';
+
+extension MemoFutureOr<R> on FutureOr<R> {
+  FutureOr<T> onResolve<T>(FutureOr<T> Function(R) onValue,
+      {Function? onError}) {
+    if (this is Future<R>) {
+      return (this as Future<R>).then(onValue);
+    } else {
+      return onValue(this as R);
+    }
+  }
+}
